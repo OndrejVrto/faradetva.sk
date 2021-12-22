2ba63c7d260245a7381246851540f95e612c0ab6
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2021-12-22 01:43:17 +0100

    run maps google

--- resources/front/views/contact.blade.php
+++ resources/front/views/contact.blade.php
@@ -17,41 +17,63 @@
 
 @push('scripts')
 <!-- map js -->
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDNdePPJKYq0ptBV_AFi_4RnmUtMa1ZLFg&libraries=places"></script>
-	<script type="text/javascript" src="asset/js/jquery.googlemap.js"></script>
-	<!-- google map script -->
 	<script>
-		$(document).ready(function() {
-			var myCenter=new google.maps.LatLng(51.508530, -0.076132);
-			function initialize()
-			{
-				var mapProp = {
-					center:myCenter,
-					zoom:8,
-					scrollwheel: false,
-					mapTypeId:google.maps.MapTypeId.ROADMAP
-				};
-				var map=new google.maps.Map(document.getElementById("contact_map"),mapProp);
-				var icon = {
-					url: 'images/icon/map_pin.png'
-				};
-				var marker=new google.maps.Marker({
-					position:myCenter,
-					map: map,
-					title: 'Church',
-					icon: icon
-				});
-				marker.setMap(map);
-				var infowindow = new google.maps.InfoWindow({
-					content:"<span> Church </span>"
-				});
-				google.maps.event.addListener(marker, 'click', function() {
-					infowindow.open(map,marker);
-				});
-			}
-			google.maps.event.addDomListener(window, 'load', initialize);
-		});
+      	// Initialize and add the map
+
+		let map;
+
+		function initMap(){
+
+			// maps center to screen and set options
+			var mapProperties = {
+				center: { lat: 48.55978989596263, lng: 19.418790178540846 },
+				zoom: 17,
+				scrollwheel: false,
+				// disableDefaultUI: true,
+				mapTypeId:google.maps.MapTypeId.HYBRID   //roadmap, satellite, hybrid and terrain
+			};
+			var map=new google.maps.Map(document.getElementById("contact_map"),mapProperties);
+
+			// set icon marker
+			var icon = {
+				url: 'images/icon/map_pin.png'
+			};
+
+			// marker Fara
+			var marker=new google.maps.Marker({
+				position: { lat: 48.559220753949724, lng: 19.419012705456197 },
+				title: 'Fara',
+				map: map,
+				icon: icon
+			});
+			marker.setMap(map);
+			var infowindow = new google.maps.InfoWindow({
+				content:"<h2> Farský úrad Detva </h2>"
+			});
+			google.maps.event.addListener(marker, 'click', function() {
+				infowindow.open(map,marker);
+			});
+
+			//marker kostol
+			var markerKostol=new google.maps.Marker({
+				position: { lat: 48.5601653909954, lng: 19.418506802091724 },
+				title: 'Kostol',
+				map: map,
+				icon: icon
+			});
+			markerKostol.setMap(map);
+			var infowindowKostol = new google.maps.InfoWindow({
+				content:"<h2> Kostol sv. Františka </h2>"
+			});
+			google.maps.event.addListener(markerKostol, 'click', function() {
+				infowindowKostol.open(map,markerKostol);
+			});
+		}
+
 	</script>
+
+	<script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBCKKEHTacfHdUwA5hnUWLvjGYaIwXX56g&callback=initMap"></script>
+
 	<!-- google map script -->
 @endpush
 
@@ -62,5 +84,7 @@
 	@include('_partials.contact')
 
 	@include('_partials.map')
+
+	@include('_partials.pasters')
 
 @endsection
