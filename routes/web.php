02d55312aded4e7b0d2a4fc0b4d47186b1a90302
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2022-01-05 10:09:43 +0100

    Only route for Login. Register is not aloved

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,22 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Backend\TagController;
 use App\Http\Controllers\Backend\NewsController;
+use App\Http\Controllers\Backend\RolesController;
+use App\Http\Controllers\Backend\UsersController;
 use App\Http\Controllers\Backend\BannerController;
 use App\Http\Controllers\Backend\PriestController;
 use App\Http\Controllers\Backend\SliderController;
-use App\Http\Controllers\Backend\CategoryController;
-use App\Http\Controllers\Backend\DashboardController;
-use App\Http\Controllers\Backend\TestimonialController;
 use App\Http\Controllers\Frontend\SearchController;
-use App\Http\Controllers\Frontend\StaticPagesController;
-use App\Http\Controllers\Backend\RolesController;
-use App\Http\Controllers\Backend\UsersController;
-use App\Http\Controllers\Backend\PermissionsController;
+use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Frontend\ArticleController;
+use App\Http\Controllers\Backend\DashboardController;
+use App\Http\Controllers\Backend\PermissionsController;
+use App\Http\Controllers\Backend\TestimonialController;
+use App\Http\Controllers\Frontend\StaticPagesController;
 
 
 /*
@@ -29,6 +30,15 @@ use App\Http\Controllers\Frontend\ArticleController;
 |
  */
 
+// Auth::routes();  //Orginal routes for Authorisation
+/** Login Routes */
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+/**  Logout Route */
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+
+
 /** Frontend Routes */
 Route::get('/', [StaticPagesController::class, 'home'] )->name('home');
 Route::get('/frantisek', [StaticPagesController::class, 'francisco'] )->name('static.francisco');
@@ -41,28 +51,9 @@ Route::get('/search/{search?}', [SearchController::class, 'search'] )->name('sea
 
 
 
-Auth::routes();
-/** Logins Routes */
-Route::group(['middleware' => 'guest'], function()
-{
-	/** Login Routes */
-	// Route::get('/login', [LoginController::class, 'show'])->name('login.show');
-	// Route::post('/login', [LoginController::class, 'login'])->name('login.perform');
-
-	/** Register Routes */
-	// Route::get('/register', [RegisterController::class, 'show'])->name('register.show');
-	// Route::post('/register', [RegisterController::class,'register'])->name('register.perform');
-});
-Route::group(['middleware' => 'auth'], function()
-{
-	/**  Logout Routes */
-	// Route::get('/logout', [LogoutController::class, 'perform'])->name('logout.perform');
-});
-
-
 /** BackEnd Routes */
-// Route::middleware(['auth', 'permission'])->prefix('admin')->group( function() {
-Route::middleware(['auth'])->prefix('admin')->group( function() {
+Route::middleware(['auth', 'permission'])->prefix('admin')->group( function() {
+// Route::middleware(['auth'])->prefix('admin')->group( function() {
 
 	Route::resource('users', UsersController::class);
 	Route::resource('roles', RolesController::class);
