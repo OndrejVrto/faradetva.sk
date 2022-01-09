5b99c7499f7a9de2db58f999189d8dedfac8c9dd
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2022-01-10 00:58:39 +0100

    add route francesco to menu

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -8,20 +8,20 @@
 
     @include('frontend._sections.banner', ['mainTitle' => 'DEBUG'])
     @include('frontend._sections.testimonials')
+    @include('frontend._sections.pray')
     @include('frontend._sections.pasters')
     @include('frontend._sections.about')
+    @include('frontend._sections.slider')
     @include('frontend._sections.aboutPage')
+    @include('frontend._sections.skill')
     @include('frontend._sections.contact')
     {{-- @include('frontend._sections.map') --}}
-    @include('frontend._sections.skill')
     {{-- @include('frontend._sections.eventExtend') --}}
     {{-- @include('frontend._sections.event') --}}
     {{-- @include('frontend._sections.ministeries') --}}
     {{-- @include('frontend._sections.news-sidebar') --}}
     @include('frontend._sections.ministeriesExtend')
-    @include('frontend._sections.slider')
-    @include('frontend._sections.news')
-    @include('frontend._sections.pray')
-    @include('frontend._sections.news-single')
+    {{-- @include('frontend._sections.news') --}}
+    {{-- @include('frontend._sections.news-single') --}}
 
 @endsection
