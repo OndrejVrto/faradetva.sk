129551cae7cb5127a7b4668f250e94c4b8b27f5b
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2022-01-09 15:35:40 +0100

    add Error page 500

--- routes/web.php
+++ routes/web.php
@@ -75,3 +75,4 @@ Route::middleware(['auth', 'permission'])->prefix('admin')->group( function()
 Route::view('419', 'errors.419');
 Route::view('403', 'errors.403');
 Route::view('404', 'errors.404');
+Route::view('500', 'errors.500');
