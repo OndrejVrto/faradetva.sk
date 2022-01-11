100215d92819dc25272512ff1c37c571f488b8ff
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2022-01-11 21:43:40 +0100

    rename component page-section

--- resources/views/frontend/_layouts/static-page.blade.php
+++ resources/views/frontend/_layouts/static-page.blade.php
@@ -5,25 +5,25 @@
 @section('keywords', 'TODO')
 
 @prepend('content_header')
+    {{-- Begin of the all static pages --}}
     @include('frontend._sections.banner', ['mainTitle' => 'TODO'])
 @endprepend
 
 @push('content_footer')
-    @include('frontend._sections.banner', ['mainTitle' => 'TODO footer'])
+    {{-- End of the all static pages --}}
 @endpush
 
+{{-- Header content static pages--}}
 @prepend('content_prepend')
-    <!-- Static page Start -->
     <div class="section static-page pad_t_50 pad_b_50">
         <div class="container">
 
-            <!-- heading Start -->
             <div class="heading_section wh_headline">
                 <h1>TODO</h1>
             </div>
-            <!-- heading End -->
 @endprepend
 
+{{-- Footer content static pages--}}
 @push('content_append')
         </div>
     </div>
