92c048df92c881902c722f622595bfb539cc79d5
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2022-01-18 20:48:13 +0100

    Add Label Source to img-componet

--- resources/views/components/page-section/img.blade.php
+++ resources/views/components/page-section/img.blade.php
@@ -1,15 +1,22 @@
 @props([
     'type' => 'right',
     'columns' => 3,
+    'source' => null,
     'alt',
-    'url'
+    'url',
 ])
 @php
-    $classes = 'img-fluid mb-3 wow rounded-3 col-md-'.($columns-1).' col-lg-'.$columns.' ';
+    $classes = 'position-relative mb-3 wow rounded-3 col-md-'.($columns-1).' col-lg-'.$columns.' ';
     $classes .= ($type == 'right') ? 'ms-sm-4 float-sm-end fromright' : 'me-sm-4 float-sm-start fromleft';
 @endphp
-<img
-    {{ $attributes->merge(['class' => $classes]) }}
+<div class="{{ $classes }}">
+    @isset($source)
+        <div class="img-article img-article-{{ $type }}">
+            {{ $source }}
+        </div>
+    @endisset
+    <img class="img-fluid"
     src="{{ $url }}"
     alt="{{ $alt }}"
-/>
+    />
+</div>
