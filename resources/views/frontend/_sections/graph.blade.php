3d85f909556b164d0b116a3dd6d2c11a000d5374
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2022-02-07 23:51:38 +0100

    Chart component

---
+++ resources/views/frontend/_sections/graph.blade.php
@@ -0,0 +1,8 @@
+<div class="col-md-9 col-lg-7 col-xl-5 m-auto">
+    <x-statistics-graph name="Krsty"/>
+    <x-statistics-graph name="Prvé sväté prijímanie"/>
+    <x-statistics-graph name="Birmovanie"/>
+    <x-statistics-graph name="Sobáše"/>
+    <x-statistics-graph name="Pohreby"/>
+    <x-statistics-graph name="Sčítanie obyvateľov Detvy"/>
+</div>
