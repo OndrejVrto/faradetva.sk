6c7bc9820af76513b0d47e12f9e0fa3825abed50
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2022-03-02 01:16:05 +0100

    Createglobal Cache for all frontend pages

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,8 +67,10 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \App\Http\Middleware\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+        'cache_response' => \App\Http\Middleware\CacheResponseMiddleware::class,
     ];
 }
--- routes/web-frontend.php
+++ routes/web-frontend.php
@@ -9,23 +9,26 @@ use App\Http\Controllers\Frontend\ContactController;
 use App\Http\Controllers\Frontend\NoticesController;
 
 //! FrontEnd Routes
-Route::get('/', HomeController::class)->name('home');
-Route::get('kontakt', ContactController::class)->name('contact');
-Route::get('oznamy', NoticesController::class)->name('notices.pdf');
+Route::middleware('cache_response')->group(function (){
 
-//! Section News article
-Route::controller(ArticleController::class)->name('article.')->group(function () {
-    Route::get('clanok/{slug}', 'show')->name('show');
-    Route::get('clanky', 'indexAll')->name('all');
-    Route::get('clanky-v-kategorii/{slug}', 'indexCategory')->name('category');
-    Route::get('clanky-podla-klucoveho-slova/{slug}', 'indexTag')->name('tag');
-    Route::get('clanky-podla-autora/{slug}', 'indexAuthor')->name('author');
-    Route::get('clanky-z-roku/{year}', 'indexDate')->where('year', '^(20\d\d)$')->name('date');
-    Route::get('hladat-clanok/{search?}', 'indexSearch')->name('search');
+    Route::get('/', HomeController::class)->name('home');
+    Route::get('kontakt', ContactController::class)->name('contact');
+    Route::get('oznamy', NoticesController::class)->name('notices.pdf');
+
+    //! Section News article
+    Route::controller(ArticleController::class)->name('article.')->group(function () {
+        Route::get('clanok/{slug}', 'show')->name('show');
+        Route::get('clanky', 'indexAll')->name('all');
+        Route::get('clanky-v-kategorii/{slug}', 'indexCategory')->name('category');
+        Route::get('clanky-podla-klucoveho-slova/{slug}', 'indexTag')->name('tag');
+        Route::get('clanky-podla-autora/{slug}', 'indexAuthor')->name('author');
+        Route::get('clanky-z-roku/{year}', 'indexDate')->where('year', '^(20\d\d)$')->name('date');
+        Route::get('hladat-clanok/{search?}', 'indexSearch')->name('search');
+    });
+
+    //! Section Search
+    Route::get('hladat/{search?}', SearchController::class)->name('search.all');
+
+    //! Section - ALL others websites
+    Route::get('{First}/{Second?}/{Third?}/{Fourth?}/{Fifth?}', PageController::class)->name('pages.others');
 });
-
-//! Section Search
-Route::get('hladat/{search?}', SearchController::class)->name('search.all');
-
-//! Section - ALL others websites
-Route::get('{First}/{Second?}/{Third?}/{Fourth?}/{Fifth?}', PageController::class)->name('pages.others');
