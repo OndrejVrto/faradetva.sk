5c25adc6c9c6ce2d3f895092ec320002c8a2babc
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2022-03-06 00:19:08 +0100

    Stub Middlerware

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,26 +6,24 @@ use Illuminate\Auth\Events\Registered;
 // use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-
-class EventServiceProvider extends ServiceProvider
 {
     /**
      * The event listener mappings for the application.
+     *
+     * @var array
      *
      * @var array
      */
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
-        ],
-    ];
-
-    /**
      * Register any events for your application.
      *
      * @return void
      */
     public function boot() {
+     *
+     * @return void
         //
     }
-}
+        //
