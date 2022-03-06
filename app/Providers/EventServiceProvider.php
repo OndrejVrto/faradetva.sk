295bc5dbe342f2a88f41f69f9c5e8c89512bac4d
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2022-03-06 01:50:51 +0100

    EventServiceProvider clear

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,15 +2,7 @@
 
 namespace App\Providers;
 
-// use App\Listeners\ResetPageCache;
-// use Illuminate\Auth\Events\Login;
-// use Illuminate\Auth\Events\Logout;
-// use Illuminate\Support\Facades\Log;
-// use Illuminate\Support\Facades\Cache;
-// use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-// use Illuminate\Auth\Events\Registered;
-// use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,26 +10,13 @@ class EventServiceProvider extends ServiceProvider
      * The event listener mappings for the application.
      */
     protected $listen = [
-        // Registered::class => [
-        //     SendEmailVerificationNotification::class,
-        // ],
-        // Logout::class => [
-        //     ResetPageCache::class,
-        // ],
+        //
     ];
 
     /**
      * Register any events for your application.
      */
     public function boot() {
-        // Event::listen([
-        //     'eloquent.created: *',
-        //     'eloquent.updated: *',
-        //     'eloquent.deleted: *',
-        //     'eloquent.restored: *',
-        // ], function($event) {
-        //     Log::info($event);
-        //     Cache::put('EXPIRE_CACHE', 'true');
-        // });
+        //
     }
 }
