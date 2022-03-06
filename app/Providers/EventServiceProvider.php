8ebb1a987171cb35fb8efef40164c03fa1002690
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2022-03-06 01:49:51 +0100

    Backup Event code in serviceProvider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,28 +2,42 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Events\Registered;
+// use App\Listeners\ResetPageCache;
+// use Illuminate\Auth\Events\Login;
+// use Illuminate\Auth\Events\Logout;
+// use Illuminate\Support\Facades\Log;
+// use Illuminate\Support\Facades\Cache;
 // use Illuminate\Support\Facades\Event;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+// use Illuminate\Auth\Events\Registered;
+// use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+
+class EventServiceProvider extends ServiceProvider
 {
     /**
      * The event listener mappings for the application.
-     *
-     * @var array
-     *
-     * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        // Registered::class => [
+        //     SendEmailVerificationNotification::class,
+        // ],
+        // Logout::class => [
+        //     ResetPageCache::class,
+        // ],
+    ];
+
+    /**
      * Register any events for your application.
-     *
-     * @return void
      */
     public function boot() {
-     *
-     * @return void
-        //
+        // Event::listen([
+        //     'eloquent.created: *',
+        //     'eloquent.updated: *',
+        //     'eloquent.deleted: *',
+        //     'eloquent.restored: *',
+        // ], function($event) {
+        //     Log::info($event);
+        //     Cache::put('EXPIRE_CACHE', 'true');
+        // });
     }
-        //
+}
