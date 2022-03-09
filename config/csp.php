de9dd95bb2e791fff7bf65f449aa7bf5476fa2d1
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2022-03-09 15:13:19 +0100

    move file CSP service

--- config/csp.php
+++ config/csp.php
@@ -2,20 +2,21 @@
 
 return [
 
-    /*
-     * A policy will determine which CSP headers will be set. A valid CSP policy is
-     * any class that extends `Spatie\Csp\Policies\Policy`
-     */
+    /**
+    * A policy will determine which CSP headers will be set. A valid CSP policy is
+    * any class that extends `Spatie\Csp\Policies\Policy`
+    **/
+    
     // 'policy' => Spatie\Csp\Policies\Basic::class,
-    'policy' => App\Services\Csp\MyCustomCspPolicy::class,
+    'policy' => App\Services\CspPolicyService::class,
 
-    /*
+    /**
      * This policy which will be put in report only mode. This is great for testing out
      * a new policy or changes to existing csp policy without breaking anything.
      */
     'report_only_policy' => '',
 
-    /*
+    /**
      * All violations against the policy will be reported to this url.
      * A great service you could use for this is https://report-uri.com/
      *
@@ -23,12 +24,12 @@ return [
      */
     'report_uri' => env('CSP_REPORT_URI', ''),
 
-    /*
+    /**
      * Headers will only be added if this setting is set to true.
      */
     'enabled' => env('CSP_ENABLED', true),
 
-    /*
+    /**
      * The class responsible for generating the nonces used in inline tags and headers.
      */
     'nonce_generator' => Spatie\Csp\Nonce\RandomString::class,
