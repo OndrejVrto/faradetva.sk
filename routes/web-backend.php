124cfebefa97e2b90151f263383d2f01cf4b8d09
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2022-03-25 16:38:30 +0100

    refactor some backend routes

--- routes/web-backend.php
+++ routes/web-backend.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Response;
 use UniSharp\LaravelFilemanager\Lfm;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\TagController;
@@ -30,73 +29,78 @@ use App\Http\Controllers\Backend\NoticeLecturerController;
 use Haruncpi\LaravelUserActivity\Controllers\ActivityController;
 
 //!  Inpersonate OUT Route
-Route::get('impersonate/leave', [ImpersonateController::class, 'leave'])->name('impersonate.leave');
 
 //!  Store files from dropzone and download zip files
-Route::post('news/media', [NewsController::class, 'storeMedia'])->name('news.storeMedia');
-Route::post('galleries/media', [GalleryController::class, 'storeMedia'])->name('galleries.storeMedia');
-Route::get('admin/galleries/{gallery}/download', [GalleryController::class, 'download'])->name('galleries.download');
+Route::prefix('admin')->group( function() {
+    Route::post('news/media', [NewsController::class, 'storeMedia'])->name('news.storeMedia');
+    Route::get('impersonate/leave', [ImpersonateController::class, 'leave'])->name('impersonate.leave');
 
-Route::middleware(['auth', 'permission'])->prefix('admin')->group( function() {
-
-    //!  Filemanager for TinyMCE Editor
-    Route::prefix('laravel-file-manager')->group( function() {
-        Lfm::routes();
+    Route::controller(GalleryController::class)->name('galleries.')->group(function () {
+        Route::post('galleries/media', 'storeMedia')->name('storeMedia');
+        Route::get('galleries/{gallery}/download', 'download')->name('download');
     });
 
-    //!  Cache and info
-    Route::controller(CacheController::class)->name('cache.')->group(function () {
-        Route::get('info-php', 'infoPHP')->name('info');
-        Route::get('caches-stop', 'cachesStop')->name('stop');
-        Route::get('caches-start', 'cachesStart')->name('start');
-        Route::get('caches-reset', 'cachesReset')->name('reset');
-        Route::get('crawl-all-url', 'crawlAllUrl')->name('crawl-all-url');
-        Route::get('caches-data-stop', 'cacheDataStop')->name('data.stop');
-        Route::get('caches-data-start', 'cacheDataStart')->name('data.start');
-        Route::get('caches-data-reset', 'cacheDataReset')->name('data.reset');
-        Route::get('failed-jobs-delete', 'deleteFailedJobs')->name('jobs.delete');
-        Route::get('failed-jobs-restart', 'restartFailedJobs')->name('jobs.restart');
+    Route::middleware(['auth', 'permission'])->group( function() {
+        //!  Main routes
+        Route::get('dashboard', DashboardController::class)->name('admin.dashboard');
+        Route::permanentRedirect('/', '/admin/dashboard');
+
+        //!  Filemanager for TinyMCE Editor
+        Route::prefix('laravel-file-manager')->group( function() {
+            Lfm::routes();
+        });
+
+        //!  Cache and info
+        Route::controller(CacheController::class)->name('cache.')->group(function () {
+            Route::get('info-php', 'infoPHP')->name('info');
+            Route::get('caches-stop', 'cachesStop')->name('stop');
+            Route::get('caches-start', 'cachesStart')->name('start');
+            Route::get('caches-reset', 'cachesReset')->name('reset');
+            Route::get('crawl-all-url', 'crawlAllUrl')->name('crawl-all-url');
+            Route::get('caches-data-stop', 'cacheDataStop')->name('data.stop');
+            Route::get('caches-data-start', 'cacheDataStart')->name('data.start');
+            Route::get('caches-data-reset', 'cacheDataReset')->name('data.reset');
+            Route::get('failed-jobs-delete', 'deleteFailedJobs')->name('jobs.delete');
+            Route::get('failed-jobs-restart', 'restartFailedJobs')->name('jobs.restart');
+        });
+
+        //!  Filemanager for Static-pages
+        Route::get('file-manager', FileManagerController::class)->name('file-manager');
+
+        //!  Inpersonate IN Route
+        Route::get('impersonate/take/{id}/{guardName?}', [ImpersonateController::class, 'take'])->whereNumber('id')->name('impersonate');
+
+        //!  Activity plugin
+        Route::controller(ActivityController::class)->name('log-activity.')->group(function () {
+            Route::get('users-activity', 'getIndex')->name('index');
+            Route::post('users-activity', 'handlePostRequest')->name('post');
+        });
+
+        //!  Resource routes
+        Route::resources([
+            'users'     => UserController::class,
+            'charts'    => ChartController::class,
+            'banners'   => BannerController::class,
+            'pictures'  => PictureController::class,
+            'galleries' => GalleryController::class,
+        ]);
+
+        Route::resources([
+            'tags'            => TagController::class,
+            'news'            => NewsController::class,
+            'files'           => FileController::class,
+            'roles'           => RoleController::class,
+            'prayers'         => PrayerController::class,
+            'priests'         => PriestController::class,
+            'sliders'         => SliderController::class,
+            'categories'      => CategoryController::class,
+            'charts.data'     => ChartDataController::class,
+            'permissions'     => PermissionController::class,
+            'static-pages'    => StaticPageController::class,
+            'testimonials'    => TestimonialController::class,
+            'notice-church'   => NoticeChurchController::class,
+            'notice-acolyte'  => NoticeAcolyteController::class,
+            'notice-lecturer' => NoticeLecturerController::class,
+        ], ['except' => 'show']);
     });
-
-    //!  Filemanager for Static-pages
-    Route::get('file-manager', FileManagerController::class)->name('file-manager');
-
-    //!  Inpersonate IN Route
-    Route::get('impersonate/take/{id}/{guardName?}', [ImpersonateController::class, 'take'])->whereNumber('id')->name('impersonate');
-
-    //!  Activity plugin
-    Route::controller(ActivityController::class)->name('log-activity.')->group(function () {
-        Route::get('users-activity', 'getIndex')->name('index');
-        Route::post('users-activity', 'handlePostRequest')->name('post');
-    });
-
-    //!  Main routes
-    Route::redirect('/', '/admin/dashboard', Response::HTTP_PERMANENTLY_REDIRECT);
-    Route::get('dashboard', DashboardController::class)->name('admin.dashboard');
-
-    Route::resources([
-        'users'     => UserController::class,
-        'charts'    => ChartController::class,
-        'banners'   => BannerController::class,
-        'pictures'  => PictureController::class,
-        'galleries' => GalleryController::class,
-    ]);
-
-    Route::resources([
-        'tags'            => TagController::class,
-        'news'            => NewsController::class,
-        'files'           => FileController::class,
-        'roles'           => RoleController::class,
-        'prayers'         => PrayerController::class,
-        'priests'         => PriestController::class,
-        'sliders'         => SliderController::class,
-        'categories'      => CategoryController::class,
-        'charts.data'     => ChartDataController::class,
-        'permissions'     => PermissionController::class,
-        'static-pages'    => StaticPageController::class,
-        'testimonials'    => TestimonialController::class,
-        'notice-church'   => NoticeChurchController::class,
-        'notice-acolyte'  => NoticeAcolyteController::class,
-        'notice-lecturer' => NoticeLecturerController::class,
-    ], ['except' => 'show']);
 });
