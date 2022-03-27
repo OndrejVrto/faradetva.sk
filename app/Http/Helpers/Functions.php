b7f6b113b5a6f9d1d2653a519345702065e0f71a
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2022-03-28 01:38:15 +0200

    start helper functions

---
+++ app/Http/Helpers/Functions.php
@@ -0,0 +1,16 @@
+<?php
+
+// if(!function_exists('canRestore'))
+// {
+
+//     function can(array $permissions): bool
+//     {
+//         return
+//             auth()->user()->hasAnyPermission($permissions)
+//             OR
+//             auth()->user()->hasRole('Super Administrátor')
+//             ? true
+//             : false;
+//     }
+
+// }
