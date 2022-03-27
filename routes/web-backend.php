4b77e4d6ea9d6206faf2ebe72f16691668ccc072
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2022-03-28 01:40:03 +0200

    Supplementing resource routes with restore and force delete methods

--- routes/web-backend.php
+++ routes/web-backend.php
@@ -76,6 +76,38 @@ Route::prefix('admin')->group( function() {
             Route::post('users-activity', 'handlePostRequest')->name('post');
         });
 
+        //!  Supplementing resource routes with restore methods
+        Route:: post('tags/{tag}/restore', [TagController::class, 'restore'])->name('tags.restore');
+        Route:: post('news/{news}/restore', [NewsController::class, 'restore'])->name('news.restore');
+        Route:: post('users/{user}/restore', [UserController::class, 'restore'])->name('users.restore');
+        Route:: post('charts/{chart}/restore', [ChartDataController::class, 'restore'])->name('charts.restore');
+        Route:: post('prayers/{prayer}/restore', [PrayerController::class, 'restore'])->name('prayers.restore');
+        Route:: post('priests/{priest}/restore', [PriestController::class, 'restore'])->name('priests.restore');
+        Route:: post('sliders/{slider}/restore', [SliderController::class, 'restore'])->name('sliders.restore');
+        Route:: post('categories/{category}/restore', [CategoryController::class, 'restore'])->name('categories.restore');
+        Route:: post('charts/{chart}/data/{data}/restore', [ChartDataController::class, 'restore'])->name('charts.data.restore');
+        Route:: post('static-pages/{static_page}/restore', [StaticPageController::class, 'restore'])->name('static-pages.restore');
+        Route:: post('testimonials/{testimonial}/restore', [TestimonialController::class, 'restore'])->name('testimonials.restore');
+        Route:: post('notice-church/{notice_church}/restore', [NoticeChurchController::class, 'restore'])->name('notice-church.restore');
+        Route:: post('notice-acolyte/{notice_acolyte}/restore', [NoticeAcolyteController::class, 'restore'])->name('notice-acolyte.restore');
+        Route:: post('notice-lecturer/{notice_lecturer}/restore', [NoticeLecturerController::class, 'restore'])->name('notice-lecturer.restore');
+
+        //!  Supplementing resource routes with force delete methods
+        Route:: post('tags/{tag}/force_delete', [TagController::class, 'force_delete'])->name('tags.force_delete');
+        Route:: post('news/{news}/force_delete', [NewsController::class, 'force_delete'])->name('news.force_delete');
+        Route:: post('users/{user}/force_delete', [UserController::class, 'force_delete'])->name('users.force_delete');
+        Route:: post('charts/{chart}/force_delete', [ChartDataController::class, 'force_delete'])->name('charts.force_delete');
+        Route:: post('prayers/{prayer}/force_delete', [PrayerController::class, 'force_delete'])->name('prayers.force_delete');
+        Route:: post('priests/{priest}/force_delete', [PriestController::class, 'force_delete'])->name('priests.force_delete');
+        Route:: post('sliders/{slider}/force_delete', [SliderController::class, 'force_delete'])->name('sliders.force_delete');
+        Route:: post('categories/{category}/force_delete', [CategoryController::class, 'force_delete'])->name('categories.force_delete');
+        Route:: post('charts/{chart}/data/{data}/force_delete', [ChartDataController::class, 'force_delete'])->name('charts.data.force_delete');
+        Route:: post('static-pages/{static_page}/force_delete', [StaticPageController::class, 'force_delete'])->name('static-pages.force_delete');
+        Route:: post('testimonials/{testimonial}/force_delete', [TestimonialController::class, 'force_delete'])->name('testimonials.force_delete');
+        Route:: post('notice-church/{notice_church}/force_delete', [NoticeChurchController::class, 'force_delete'])->name('notice-church.force_delete');
+        Route:: post('notice-acolyte/{notice_acolyte}/force_delete', [NoticeAcolyteController::class, 'force_delete'])->name('notice-acolyte.force_delete');
+        Route:: post('notice-lecturer/{notice_lecturer}/force_delete', [NoticeLecturerController::class, 'force_delete'])->name('notice-lecturer.force_delete');
+
         //!  Resource routes
         Route::resources([
             'users'     => UserController::class,
