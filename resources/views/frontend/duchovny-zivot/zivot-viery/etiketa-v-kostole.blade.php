e2b28e3a9cb801984a4883121d89debec7dedeeb
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-04-10 20:09:33 +0200

    New Texts from Marian

---
+++ resources/views/frontend/duchovny-zivot/zivot-viery/etiketa-v-kostole.blade.php
@@ -0,0 +1,106 @@
+<x-frontend.layout.master :pageData="$pageData">
+
+    {{-- Etiketa platí aj v kostole --}}
+
+    <x-frontend.page.section name="PAGE: ({{$pageData['title']}}) -" class="static-page pad_b_50">
+
+        <x-frontend.page.subsection>
+            {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4" /> --}}
+            <x-frontend.page.text-segment animation="fromleft">
+                <p>
+                    Človek, ktorý vchádza do kostola a chce sa zúčastniť na slávení svätej omše, by mal byť čisto, vkusne a aj trochu sviatočne oblečený. Dámy by sa mali vyhnúť mikrosukniam a hlbokým výstrihom, páni krátkym nohaviciam. To, čo si dnes najmä v lete ľudia obliekajú do kostola, je vhodné skôr na kúpalisko. Možno niekto namietne, že vo svojom voľnom čase sa môže obliecť, ako chce. No nie je účasť na svätej omši slávnostným a radostným trávením voľného času? Nie je to svetlý bod všedného života? Tomu by potom malo zodpovedať oblečenie a aj naše správanie.
+                </p>
+            </x-frontend.page.text-segment>
+        </x-frontend.page.subsection>
+
+        <x-frontend.page.subsection title="Ako je to so sedením v lavici?">
+            {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4" /> --}}
+            <x-frontend.page.text-segment>
+                <p>
+                    Na kraji sedí muž, žena by tam nemala sedieť. Ak vchádzajú muž a žena do lavice, ktorá je už skraja obsadená, prvý do lavice vstupuje muž, aby žene uvoľňoval cestu. K sediacim ľuďom sa otáčame tvárou. Lavice v kostoloch sú však zvyčajne úzke, preto by sa mali obsadzovať zo stredu, nie od kraja.
+                </p>
+                <p>
+                    Nie je slušné otáčať sa a obzerať si človeka, ktorý vchádza do kostola. Je na mieste otázka, prečo sú ľudia, ktorí sa otočili, v kostole? Preto, aby videli, kto do chrámu vošiel? Sú tam pre klebety alebo pre Pána Ježiša prítomného v Eucharistii? Účasť na svätej omši nie je o tom, že idem do kostola, lebo nemám čo iné robiť, a potom všetkých ohodnotím, aké tá mala šaty, topánky, ako bola namaľovaná, ako mala upravené vlasy a pod.
+                </p>
+                <p>
+                    Sedieť v kostole treba spoločensky vhodne a uvoľnene, nie upäto. Nohy by mali byť pod kľačadlom. Kľačadlo je na kľačanie, preto sa aj tak volá, nie je to opierka na nohy. Avšak keď niektorí ľudia „kľačia“, kolená majú na kľačadle, zadnou časťou tela sa ale opierajú o lavicu, na ktorej predtým sedeli, to nie je kľačanie. Kľačanie je, keď sa celou váhou tela opieram o kolená a nepodopieram sa sedacou časťou tela o lavicu. Ak má človek choré kĺby alebo akékoľvek iné zdravotné problémy, môže zostať sedieť. Nemusí stáť ani počas tých častí svätej omše, pri ktorých sa státie vyžaduje. Keď ma totiž niečo bolí, koncentrujem sa na bolesť, a v kostole by sme sa mali koncentrovať na stretnutie s Pánom.
+                </p>
+                <p>
+                    Do chrámov prichádzajú aj ľudia, ktorí sú zvyknutí na svoje „stále“ miesto v kostole. Nárokovať si na lavicu v kostole súvisí s tým, že človek sa na istom mieste cíti príjemne, akoby bol doma. Tak ako si v obývačke obsadia niektorí ľudia kreslo alebo diaľkový ovládač k televízoru, tak si daktorí vo svojej mysli zarezervujú aj verejné priestranstvo - lavičku v parku alebo sedadlo v kostole, ktoré sa tým stáva ich osobným územím. Ak niekto iný obsadí toto ich teritórium, vyvolá v nich napätie, nepokoj, nervozitu. Takto uvažujúci človek nebude spokojný, kým nebude sedieť na svojom mieste. Na druhej strane však nemá právo vynútiť si ho. Menovky ani rezervácie na miesta v kostoloch nejestvujú, nie sme v divadle ani v reštaurácii. V sporoch o miesto na sedenie spravidla ustúpi ten, kto je ľudskejší a empatickejší. Nech už sú pohnútky niektorých akékoľvek, kostol ani sväté omše nie sú nato, aby sme sa sporili, kde budeme sedieť či stáť, a ani nato, aby sme si vyhradzovali miesto na sedenie či státie.
+                </p>
+                <p>
+                    Keď niekto príde neskôr na svätú omšu a chce si sadnúť, ale voľné miesto je len v prvej lavici, mal by dbať o to, aby nerušil ostatných, ktorí prišli včas. Treba sa snažiť ísť potom krajom, nie stredom kostola, ani rýchlo, ani pomaly, ak ženám klopkajú opätky, mali by sa snažiť ísť po špičkách. Každý by sa však mal usilovať stihnúť začiatok svätej omše, aj keď nie vždy je to z rozličných príčin možné.
+                </p>
+            </x-frontend.page.text-segment>
+        </x-frontend.page.subsection>
+
+        <x-frontend.page.subsection title="Ako reagovať na rušivé vplyvy?" animation="fromleft">
+            {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4" /> --}}
+            <x-frontend.page.text-segment animation="fromleft">
+                <p>
+                    Ako upozorniť ľudí, ktorí sa pred nami alebo za nami počas svätej omše rozprávajú? Všetko je o komunikácii. Rozprávajúcich sa treba upozorniť vhodne, čo najkratšie a najúctivejšie, aj s milým úsmevom. Nikdy totiž nevieme, prečo a o čom sa môžu rozprávať, hoci by sa rozprávať nemali.
+                </p>
+                <p>
+                    Okrem hlasného hovoru nás môžu rušiť v kostole aj iné veci. Ľuďom v našom okolí môže byť na ťarchu cigaretový zápach, výrazný parfum, pach tela, nepríjemný dych, zápach oblečenia, hlasný hovor, uprený pohľad, lebo to všetko zasahuje do ich osobnej až intímnej zóny. Aj milovník cesnaku či cibule by ich mal konzumovať až po svätej omši.
+                </p>
+                <p>
+                    Elementárna slušnosť káže vypnúť si pred vstupom do kostola mobilný telefón. Často sa však stáva, že počas svätej omše niekomu telefón zazvoní, ba dokonca niekto v kostole aj telefonuje. Môže sa to prihodiť, s tým treba počítať, sme jednoducho ľudia, sme omylní. Aj pri zvonení cudzích mobilných telefónov však ide v prvom rade o našu osobnú koncentráciu na svätú omšu. No prijať hovor alebo posielať SMS správy v kostole je neslušné!
+                </p>
+            </x-frontend.page.text-segment>
+        </x-frontend.page.subsection>
+
+        <x-frontend.page.subsection title="Na čo by sme mali dávať pozor?">
+            {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4" /> --}}
+            <x-frontend.page.text-segment>
+                <p>
+                    Keď v kostole zbadáme známeho, ktorý je od nás ďaleko a chceme ho pozdraviť, stačí ho skontaktovať očami, pousmiať sa, kývnuť hlavou. Keď sa s ním chceme rozprávať, môžeme ho počkať po svätej omši a vonku si pohovoriť. V kostole nie. Kostol nie je kaviareň.
+                </p>
+                <p>
+                    Nikto nemôže namietať, keď niekto zostane stáť vonku pred dverami kostola vtedy, ak sa dnu nezmestí, alebo má malé dieťa, ktoré by ostatných vyrušovalo, alebo ak je vo vnútri vydýchaný vzduch. Ale zúčastniť sa svätej omše tak, že ani nevojdem do kostola, iba stáť neďaleko neho v okruhu niekoľkých desiatok metrov, s niekým debatovať či fajčiť, je nemysliteľné. To potom možno sedieť v každom pohostinstve pri kostole a človek bude účastný na svätej omši? Určite nie.
+                </p>
+                <p>
+                    Ak má človek invalidný vozík, barly alebo inú pomôcku, bez ktorej sa ťažko pohybuje, mal by ich umiestniť tak, aby neprekážali iným veriacim. Platí to aj pri batohoch či iných taškách, treba ich uložiť tak, aby nezavadzali ostatným ľuďom. To si od človeka vyžaduje, aby bol dobrým pozorovateľom a vedel odhadnúť situáciu i priestorové možnosti. S nákupom ani s batožinou netreba chodiť hore-dolu po laviciach, ani nimi netreba zablokovať lavicu skraja.
+                </p>
+                <p>
+                    Mokrý dáždnik v nijakom prípade nekladieme pod nohy inému človeku. Odporúča sa vložiť mokrý dáždnik, ak je skladací, do igelitovej tašky alebo vrecúška. Takto zabalený ho môžeme dať aj do kabelky. Keď máme veľký dáždnik, ktorý sa nevmestí do igelitovej tašky, alebo ju nemáme so sebou, treba starostlivo zvažovať, kam ho v kostole položíme či oprieme. Na lavicu alebo drevenú podlahu mokrý dáždnik nepatrí.
+                </p>
+                <p>
+                    Kabelka podľa spoločenskej etikety nepatrí na zem ani na stôl. Ak na laviciach nie sú háčiky, treba ju položiť vedľa seba na lavicu alebo ju držať v lone. V kostole by sme nemali mať na sebe slnečné okuliare, tie do miestnosti nepatria. Ani na očiach, ani na vrchu hlavy ako čelenku. Treba ich mať v puzdre. Tvrdenie, že na vrchu hlavy sú ozdoba a módny doplnok, z pohľadu spoločenskej etikety neobstojí. No keď je človek chorý, môže mať slnečné okuliare aj v miestnosti.
+                </p>
+                <p>
+                    Pokrývka hlavy, zvlášť u mužov, nie je v miestnosti žiaduca. Ak je však človek chorý alebo ide o malé dieťa a rodič sa bojí, aby neochorelo, môže mať pokrývku hlavy. V našom kultúrnom kontexte by dámy nemali do kostola nosiť obrovské klobúky. Tie sú vhodné skôr na dostihy. Každý zážitok, aj zážitok zo svätej omše, je vtedy pozitívny, keď sa do jeho vnímania zapojí viac zmyslov - ak nielen počujeme, ale aj vidíme a pritom nás neruší silný zápach či parfum, alebo obrovský klobúk zasahujúci do nášho teritória.
+                </p>
+                <p>
+                    Nie je správne, keď kňaz aj s miništrantami po svätej omši odchádzajú, a hoci sú ešte stále pri oltári, ľudia sa postavia a idú preč. Pripomína to záver divadelného predstavenia, keď niektorí diváci utekajú do šatne po kabáty. Kam sa náhlia? Nedokážu vychutnať doznievanie povznášajúcich pocitov? Podľa etikety najskôr odchádza kňaz s miništrantami a až po nich veriaci.
+                </p>
+            </x-frontend.page.text-segment>
+        </x-frontend.page.subsection>
+
+        <x-frontend.page.subsection title="Praktické rady" animation="fromleft">
+            {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4" /> --}}
+            <x-frontend.page.text-segment animation="fromleft">
+                <p>
+                    Rodiny s malými deťmi by sa nemali vyčleňovať spomedzi iných veriacich spôsobom, že na jednej strane budú ženy, na druhej muži a inde rodiny. Každý rodič by mal sám odhadnúť, či jeho dieťa bude pokojné, a prípadne by si mal sadnúť tak, aby s plačúcim dieťaťom mohol rýchlo z kostola vyjsť. Občas sa stáva, že mladí rodičia nechávajú svoje deti počas bohoslužieb vykrikovať, zabávať sa, behať po kostole. Mali by však svoje deti viesť k tomu, že kostol nie je miesto na hranie, ale miesto na oslavu Boha a stretnutia sa s ním.
+                </p>
+                <p>
+                    Tak ako v banke, na pošte a všade, kde sa prichádza do styku so súkromnými informáciami, aj pri spovednici platí dodržiavanie osobnej zóny. Ba pri sviatosti zmierenia ešte viac, pretože tieto rozhovory nesmie počuť nikto iný. Treba pamätať na dodržiavanie diskrétnej vzdialenosti od spovednice. Sledovanie času spovedania sa iných veriacich sú zvláštne praktiky, ktoré nepatria do kostola.
+                </p>
+                <p>
+                    Stáva sa, že kňaz už stojí s Božím telom, ktoré ide podať veriacim k svätému prijímaniu a musí hodnú chvíľu čakať, kým prídu, lebo ešte len začínajú vychádzať z lavíc. To je jeden extrém, druhý je, keď kňaz ešte len ide k svätostánku po konsekrované hostie a ľudia sa už hrnú dopredu hlava-nehlava. Chýba potom pátos a noblesa. Treba nájsť konsenzus, medzičas, aby kňaz dlho nestál s Ježišom prítomným v Eucharistii pred oltárom a aby ľudia pridlho nesedeli v laviciach.
+                </p>
+                <p>
+                    Každý veriaci by mal prispievať milodarmi podľa svojich možností. Keď si niekto môže dovoliť dať na kostol sto eur, vďaka Bohu, že mu Pán požehnal túto možnosť. Mali by sme ale pamätať, že každý náš milodar by mal predstavovať nezištné konanie v prospech iného a na úkor seba. Bohu je milšie, keď dá niekto aj posledný cent, ako keď dá niekto tisíc eur, ktoré mu vôbec nebudú chýbať. Ak nemáme peniaze - a čo je dôležitejšie - ani dobrý úmysel, radšej neprispejme ničím. Keď však zazrieme, koľko peňazí niekto vhodil do zvončeka, nesmieme sa tým zaoberať, nemôžeme posudzovať. Nikdy nevieme, prečo ten človek hodil málo alebo veľa. A čo je vlastne málo alebo veľa?
+                </p>
+                <p>
+                    Fotograf alebo kameraman, ktorý fotografuje alebo nakrúca záznam z prvého svätého prijímania, birmovania, zo svadby, z krstu či pohrebu, by sa mal správať tak, aby nerušil ostatných veriacich. Dobrý fotograf alebo kameraman je ten, koho takmer nevidíme, akoby tam ani nebol. Aj v kostole existuje istá zóna, do ktorej už fotograf a kameraman nemôže vstúpiť, a to by mal rešpektovať. Okrem toho by sa mal slušne obliecť a obuť si neklopkajúce topánky.
+                </p>
+            </x-frontend.page.text-segment>
+        </x-frontend.page.subsection>
+
+
+        <x-frontend.page.information-sources title="Použitá literatúra:">
+            <li>Spoločenská etiketa platí aj v kostole. Príloha Katolíckych novín, 2011.</li>
+        </x-frontend.page.information-sources>
+
+    </x-frontend.page.section>
+</x-frontend.layout.master>
