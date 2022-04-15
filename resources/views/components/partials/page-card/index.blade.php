9b4c395d0315a7819785dc455859831508db6fbf
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-04-15 23:54:02 +0200

    update page card component

--- resources/views/components/partials/page-card/index.blade.php
+++ resources/views/components/partials/page-card/index.blade.php
@@ -1,7 +1,13 @@
 <div class="col-md-6 col-lg-4">
     <div class="ministry_box fromright wow" data-wow-delay="{{ $delay }}">
         <div class="ministry_thumb">
-            {!! $pageCard['responsivePicture'] !!}
+
+            {{-- {!! $pageCard['responsivePicture'] !!} --}}
+            <img src="{{ $pageCard['img-section-list'] }}"
+                class="img-fluid"
+                alt="{{ $pageCard['img-description'] }}"
+            />
+
             <span class="d-none" rel="license" for="Ministry_{{ $pageCard['title'] }}">
                 <x-partials.source-sentence
                     :sourceArray="$pageCard['sourceArr']"
@@ -11,7 +17,7 @@
         <div class="ministry_desc">
             <h4>{{ $pageCard['title'] }}</h4>
             <p>{{ $pageCard['teaser'] }}</p>
-            <a href="{{ $pageCard['url_page'] }}" class="join_btn read_btn">Viac o spoločenstve</a>
+            <a href="{{ $pageCard['url'] }}" class="join_btn read_btn">Viac o spoločenstve</a>
         </div>
     </div>
 </div>
