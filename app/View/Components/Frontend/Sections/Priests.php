050825aeb5fe36c0a7ec5b8a800880c8f667e03d
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-04-15 23:58:38 +0200

    whitespace update

--- app/View/Components/Frontend/Sections/Priests.php
+++ app/View/Components/Frontend/Sections/Priests.php
@@ -31,14 +31,14 @@ class Priests extends Component
                 ->get()
                 ->map(function($priest): array {
                     return [
-                        'id'               => $priest->id,
-                        'full_name_titles' => $priest->full_name_titles,
-                        'phone'            => $priest->phone,
-                        'phone_digits'     => $priest->phone_digits,
-                        'email'            => $priest->email,
-                        'function'         => $priest->function,
-                        'img-url'          => $priest->getFirstMediaUrl('priest', 'crop'),
-                        'description'      => (new PurifiAutolinkService)->getCleanTextWithLinks($priest->description),
+                        'id'                => $priest->id,
+                        'full_name_titles'  => $priest->full_name_titles,
+                        'phone'             => $priest->phone,
+                        'phone_digits'      => $priest->phone_digits,
+                        'email'             => $priest->email,
+                        'function'          => $priest->function,
+                        'img-url'           => $priest->getFirstMediaUrl('priest', 'crop'),
+                        'description'       => (new PurifiAutolinkService)->getCleanTextWithLinks($priest->description),
                     ];
             })->toArray();
         });
