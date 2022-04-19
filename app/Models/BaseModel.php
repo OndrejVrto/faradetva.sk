d2470defd0433fa3fe957daaf0120d5a9ff9fd32
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-04-19 15:46:31 +0200

    Create BaseModel

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Touhidurabir\ModelSanitize\Sanitizable;
+
+class BaseModel extends Model
+{
+    use Sanitizable;
+
+    public function getRouteKeyName() {
+        return 'slug';
+    }
+}
