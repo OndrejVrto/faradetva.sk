876d708952baaccb7811d4217ccaa91b83365cfe
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-04-20 15:13:21 +0200

    refactor component for minimalise queries to DB

--- app/Http/Helpers/Functions.php
+++ app/Http/Helpers/Functions.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Arr;
+
 // if(!function_exists('canRestore'))
 // {
 
@@ -12,5 +14,29 @@
 //             ? true
 //             : false;
 //     }
-
 // }
+
+if(!function_exists('prepareInput'))
+{
+    function prepareInput($input): ?array {
+        if (!$input) {
+            return null;
+        }
+
+        if (is_string($input)) {
+            return array_map('trim', explode(',', $input));
+        }
+
+        if (is_array($input)) {
+            return array_filter(Arr::flatten($input));
+        }
+    }
+}
+
+if (!function_exists('getCacheName'))
+{
+    function getCacheName(array $listOfItems): string
+    {
+        return md5(implode('|', $listOfItems));
+    }
+}
--- app/View/Components/Partials/PageCard.php
+++ app/View/Components/Partials/PageCard.php
@@ -9,43 +9,45 @@ use Illuminate\Support\Facades\Cache;
 
 class PageCard extends Component
 {
-    public $pageCard;
+    public $pageCards = null;
 
     public function __construct(
-        public string $routeStaticPage,
-        public string $delay
+        public $routeStaticPages = null,
     ) {
-        $this->pageCard = Cache::rememberForever('PAGE_CARD_'.$routeStaticPage, function () use($routeStaticPage) {
-            return StaticPage::where('route_name',$routeStaticPage)->with('mediaOne', 'source')->get()->map(function($page) {
-                $colectionName = $page->mediaOne->collection_name;
-                $media = $page->getFirstMedia($colectionName);
+        $listOfPages = prepareInput($routeStaticPages);
 
-                return  [
-                    'id'              => $page->id,
-                    'author'          => $page->author_page,
-                    'description'     => $page->description_page,
-                    'header'          => $page->header,
-                    'keywords'        => $page->keywords,
-                    'slug'            => $page->slug,
-                    'teaser'          => $page->teaser,
-                    'title'           => $page->title,
-                    'url'             => $page->full_url,
-                    'wikipedia'       => $page->wikipedia,
+        if ($listOfPages) {
+            $cacheName = getCacheName($listOfPages);
 
-                    'img-section-list'=> $media->getUrl('section-list'),
-                    'img-description' => $page->source->description,
+            $this->pageCards = Cache::rememberForever('PAGE_CARDS_'.$cacheName, function () use($listOfPages) {
+                return StaticPage::whereIn('route_name',$listOfPages)->with('mediaOne', 'source')->get()->map(function($page) {
+                    return  [
+                        'id'              => $page->id,
+                        'author'          => $page->author_page,
+                        'description'     => $page->description_page,
+                        'header'          => $page->header,
+                        'keywords'        => $page->keywords,
+                        'slug'            => $page->slug,
+                        'teaser'          => $page->teaser,
+                        'title'           => $page->title,
+                        'url'             => $page->full_url,
+                        'wikipedia'       => $page->wikipedia,
 
-                    'sourceArr' => [
-                        'source'      => $page->source->source,
-                        'source_url'  => $page->source->source_url,
-                        'author'      => $page->source->author,
-                        'author_url'  => $page->source->author_url,
-                        'license'     => $page->source->license,
-                        'license_url' => $page->source->license_url,
-                    ],
-                ];
-            })->first();
-        });
+                        'img-section-list'=> $page->mediaOne->getUrl('section-list'),
+                        'img-description' => $page->source->description,
+
+                        'sourceArr' => [
+                            'source'      => $page->source->source,
+                            'source_url'  => $page->source->source_url,
+                            'author'      => $page->source->author,
+                            'author_url'  => $page->source->author_url,
+                            'license'     => $page->source->license,
+                            'license_url' => $page->source->license_url,
+                        ],
+                    ];
+                });
+            });
+        }
     }
 
     public function render(): View {
