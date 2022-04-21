48ba1769d4ad2bd8947834d6a08c707e6cc63118
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-04-21 16:06:23 +0200

    refactor component in page "spolocenstva"

--- resources/views/frontend/spolocenstva/index.blade.php
+++ resources/views/frontend/spolocenstva/index.blade.php
@@ -4,12 +4,13 @@
 
     <x-frontend.page.section-header :header="$pageData['header']" class=""/>
 
-        <x-partials.page-card delay=".2s" routeStaticPage="spolocenstva.frantiskansky-svetstky-rad" />
-        <x-partials.page-card delay=".4s" routeStaticPage="spolocenstva.marianske-veceradlo" />
-        <x-partials.page-card delay=".6s" routeStaticPage="spolocenstva.rad-bosych-karmelitanok" />
-        <x-partials.page-card delay=".8s" routeStaticPage="spolocenstva.ruzencove-bratstvo" />
-        <x-partials.page-card delay="1s"  routeStaticPage="spolocenstva.svetsky-rad-bosych-karmelitanov" />
-        <x-partials.page-card delay="1.2s" routeStaticPage="spolocenstva.zdruzenie-salezianskych-spolupracovnikov" />
-
+        <x-partials.page-card routeStaticPages="
+                spolocenstva.frantiskansky-svetstky-rad
+                spolocenstva.marianske-veceradlo,
+                spolocenstva.rad-bosych-karmelitanok,
+                spolocenstva.ruzencove-bratstvo,
+                spolocenstva.svetsky-rad-bosych-karmelitanov,
+                spolocenstva.zdruzenie-salezianskych-spolupracovnikov
+            " />
     </x-frontend.page.section>
 </x-frontend.layout.master>
