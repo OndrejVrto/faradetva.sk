a921769b5193cc36cdfef0b0a9b9199979e24554
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-05-01 16:42:39 +0200

    clear comments in rules

--- app/Rules/SingleWord.php
+++ app/Rules/SingleWord.php
@@ -6,31 +6,14 @@ use Illuminate\Contracts\Validation\Rule;
 
 class SingleWord implements Rule
 {
-    /**
-     * Create a new rule instance.
-     *
-     * @return void
-     */
     public function __construct() {
         //
     }
 
-    /**
-     * Determine if the validation rule passes.
-     *
-     * @param  string  $attribute
-     * @param  mixed  $value
-     * @return bool
-     */
     public function passes($attribute, $value) {
         return is_string($value) && ! preg_match('/\s/u', $value);
     }
 
-    /**
-     * Get the validation error message.
-     *
-     * @return string
-     */
     public function message() {
         return 'Pole môže obsahovať len jediné slovo.';
     }
