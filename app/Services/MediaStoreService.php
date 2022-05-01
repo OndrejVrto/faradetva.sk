8de128f4e3a15109ed8ea96d0ff81ee189969f51
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-05-01 22:55:14 +0200

    change filename properties in store media service

--- app/Services/MediaStoreService.php
+++ app/Services/MediaStoreService.php
@@ -28,7 +28,9 @@ class MediaStoreService
         if (!empty($request->crop_base64_output) AND !empty($request->crop_file_name)) {
             $colectionName = $model->collectionName;
             $fileExtension = explode("image/", explode(";base64,", $request->crop_base64_output)[0])[1];
-            $fileName = Str::slug($request->description).'.'.$fileExtension;
+            $fileName = isset($request->description)
+                ? Str::slug($request->description).'.'.$fileExtension
+                : DataFormater::filterFilename($request->crop_file_name, true);
 
             $model->clearMediaCollection($colectionName);
             $model->addMediaFromBase64($request->crop_base64_output)
