3cac4ca2fe745a63a558cee9c555dbd1f8726b02
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-05-01 13:14:13 +0200

    Crop in modal window

--- resources/views/components/backend/form/crop.blade.php
+++ resources/views/components/backend/form/crop.blade.php
@@ -5,31 +5,77 @@
     'maxSize'   => "2600*1600",
 ])
 <!--  Component: CROP - Start -->
-{{-- toto je container kde ma fungovat cropper --}}
-<div class="preview-container">
-    <img id="cropperElement" class="d-none">
-</div>
-
-{{-- toto je button na vytvorenie cropu --}}
-<div class="crop-container">
-    <button id="cropButton" type="button" class="btn bg-gradient-warning d-none">
-        Orezať
-    </button>
-</div>
-
-{{-- toto je preview container toho co sa prave nahrava --}}
-<div class="preview-container">
-    <img id="preview" class="d-none">
-</div>
 
 {{-- toto je vstupny input pre subor --}}
-<label for="uploadFileInput" class="upload-area" id="uploadFileContainer">
-    <input id="uploadFileInput" name="upload-file" type=file>
-</label>
+<x-adminlte-input-file
+    class="border-right-none"
+    name="upload-crop-file"
+    id="upload-corp-file-input"
+    label="Obrázok"
+    {{-- placeholder="{{ $media_file_name }}" --}}
+    accept=".jpg,.bmp,.png,.jpeg"
+>
+    <x-slot name="prependSlot">
+        <div class="input-group-text bg-gradient-orange">
+            <i class="fas fa-file-import"></i>
+        </div>
+    </x-slot>
+    <x-slot name="noteSlot">
+        Poznámka: veľkosť obrázka minimálne {{ $minWidth }}x{{ $minHeight }} px.
+    </x-slot>
+</x-adminlte-input-file>
 
 {{-- toto je hidden input field kde sa ulozi finalna base64 --}}
-<input id="photo-base64" name="photo-base64" type="text" hidden>
+<input id="crop-base64-output" name="crop-base64-output" type="text" hidden>
+
+{{-- toto je preview container toho co sa prave nahrava --}}
+<div class="form-group ">
+    <label for="title">Náhľad</label>
+    <div class="preview-container">
+        <img id="crop-preview">
+    </div>
+</div>
+
+{{-- toto je modálne okno pre zobrazenie croppera --}}
+<div class="modal" id="cropModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-xl">
+        <div class="modal-content">
+            <div class="modal-body">
+                {{-- toto je container kde ma fungovat cropper --}}
+                <div class="crop-container">
+                    <img id="cropper-element">
+                </div>
+            </div>
+            <div class="modal-footer">
+                {{-- toto je button na vytvorenie cropu --}}
+                <button id="crop-button" type="button" class="btn bg-gradient-orange px-5 mr-2">
+                    <i class="fa-solid fa-crop-simple mr-1"></i>
+                    Orezať
+                </button>
+                {{-- toto je button cancel --}}
+                <button id="crop-cancel-button" type="button" class="btn bg-gradient-danger px-5">
+                    <i class="fa-solid fa-ban mr-1"></i>
+                    Zrušiť
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
 <!--  Component: CROP - End -->
+
+@push('css')
+    <link @nonce rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" >
+    <style>
+        .preview-container img {
+            /* max-height: {{ $minHeight }}px; */
+            object-fit: scale-down;
+            width: 100%;
+        }
+        .crop-container {
+            max-height: 600px;
+        }
+    </style>
+@endpush
 
 @push('js')
     {{-- tento skript tu ostane ako konkretne volanie funkcie --}}
@@ -39,92 +85,115 @@
             minHeight: {{ $minHeight }},
             ratio: {{ $ratio }},
             maxSize: {{ $maxSize }},
-            input: '#uploadFileInput',
-            inputContainer: '#uploadFileContainer',
-            output: '#photo-base64',
-            preview: '#preview',
-            cropperContainer: '#cropperElement',
-            cropButton: '#cropButton'
+            input: '#upload-corp-file-input',
+            output: '#crop-base64-output',
+            preview: '#crop-preview',
+            cropperContainer: '#cropper-element',
+            cropButton: '#crop-button',
+            cancelCropButton: '#crop-cancel-button'
         });
     </script>
 @endpush
 
 @prepend('js')
-    <script @nonce src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.js"></script>
+    <script @nonce src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.js" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     {{-- tento skript definuje to co je potrebne a mozes si ho vytiahnut do suboru --}}
     <script @nonce>
         /* config args: minWidth, minHeight, ratio, maxSize, input, output, preview, cropperContainer, cropButton */
         function watchImageUploader(config) {
-    
+
             function showResult() {
-                $(config.cropButton).addClass('d-none');
-                $(config.cropperContainer).addClass('d-none');
-                $(config.preview).removeClass('d-none');
+                // $(config.cropButton).addClass('d-none');
+                // $(config.cancelCropButton).addClass('d-none');
+                // $(config.cropperContainer).addClass('d-none');
+                // $(config.preview).removeClass('d-none');
             }
-    
+
+            function cancelCroper() {
+                // $(config.cropButton).addClass('d-none');
+                // $(config.cancelCropButton).addClass('d-none');
+                // $(config.cropperContainer).addClass('d-none');
+                $('#cropModal').modal('hide');
+                if (!$(config.preview).attr("src")) {
+                    $(config.input).val('');
+                    $('.custom-file-label').empty();
+                }
+            }
+
             function showCropper() {
-                $(config.input).addClass('d-none');
-                $(config.inputContainer).addClass('d-none');
-                $(config.cropButton).removeClass('d-none');
-                $(config.cropperContainer).removeClass('d-none');
+                $('#cropModal').modal('show');
+                // $('.iframe-full-height').on('load', function(){
+                //     this.style.height=this.contentWindow.document.body.scrollHeight + 'px'
+                // });
+                // $('#cropModal').modal({
+                //     keyboard: false
+                // }).focus();
+                // $(config.input).addClass('d-none');
+                // $(config.preview).addClass('d-none');
+                // $(config.cropButton).removeClass('d-none');
+                // $(config.cancelCropButton).removeClass('d-none');
+                // $(config.cropperContainer).removeClass('d-none');
             }
-    
+
             function setToForm(base64) {
                 $(config.output).val(base64);
                 $(config.preview).attr("src", (base64));
-                showResult();
+                $('#cropModal').modal('hide');
+                // $(config.input).removeClass('d-none');
+                // showResult();
             }
-    
+
             function checkDimensions(img) {
                 if (img.width < config.minWidth || img.height < config.minHeight) {
-                    alert('Obrazok nesplna minimalne rozmery');
+                    alert('Obrázok má malé rozmery:\n\nŠírka najmenej '+ config.minWidth +'px\nVýška najmenej '+ config.minHeight +'px');
+                    $('.custom-file-label').empty();
                     return false;
                 }
                 return true;
             }
-    
+
             function readFile(file) {
                 const reader = new FileReader();
-    
+
                 reader.onload = (event) => {
                     const img = document.createElement('img');
-    
+
                     img.onload = () => {
-    
+
                         if (!checkDimensions(img)) {
                             return;
                         }
-    
+
                         showCropper();
-    
+
                         const cropperEntryPoint = $(config.cropperContainer)[0];
-    
+
                         cropperEntryPoint.src = reader.result;
-    
+
                         if(config.ratio == true) {
                             ratio = config.minWidth / config.minHeight;
                         } else {
                             ratio = null;
                         }
-    
+
+                        var cropBoxData;
+                        var canvasData;
+
                         cropper = new Cropper(cropperEntryPoint, {
                             aspectRatio: ratio,
-                            viewMode: 1,
+                            viewMode: 2,
+                            modal: true,
+                            center: true,
                             zoomable: false,
                             movable: false,
                             rotatable: false,
                             scalable: false,
-                            
-                            data: {
-                                width: config.minWidth,
-                                height: config.minHeight,
-                            },
-    
+
                             crop: function (event) {
                                 var width = event.detail.width;
                                 var height = event.detail.height;
-    
+
                                 if (
                                     width < config.minWidth
                                     || height < config.minHeight
@@ -135,126 +204,65 @@
                                     });
                                 }
                             },
-    
+
                         });
-    
+
+                        $(config.cancelCropButton).click(() => {
+                            cropper.destroy();
+                            cancelCroper();
+                            return;
+                        });
+
                         $(config.cropButton).click(() => {
                             cropper.crop();
-    
+
                             const base64 = cropper.getCroppedCanvas({fillColor: '#fff'})
                                 .toDataURL(file.type);
-    
+
                             const cropped = document.createElement('img');
-    
+
                             cropped.onload = () => {
                                 if (!checkDimensions(cropped)) {
                                     return;
                                 }
-    
+
                                 const croppedSize = cropped.width * cropped.height;
-                        
+
                                 if (croppedSize > config.maxSize) {
                                     const reduction = Math.sqrt(croppedSize / config.maxSize);
                                     const finalWidth = cropped.width / reduction;
                                     const finalHeight = cropped.height / reduction;
-                            
+
                                     const canvas = document.createElement('canvas');
                                     canvas.width = finalWidth;
                                     canvas.height = finalHeight;
-                            
+
                                     const ctx = canvas.getContext('2d');
-                            
+
                                     if (ctx) {
                                         ctx.drawImage(cropped, 0, 0, finalWidth, finalHeight);
                                         setToForm(canvas.toDataURL(file.type));
                                     }
-    
                                 } else {
                                     setToForm(base64);
                                 }
-    
+
                                 cropper.destroy();
                             }
-    
+
                             cropped.src = base64;
-    
                         });
                     };
-    
+
                     img.src = event.target?.result;
                 };
-    
+
                 reader.readAsDataURL(file);
             }
-    
+
             $(config.input).on('change', (e) => {
                 readFile($(config.input)[0].files[0]);
             });
-    
-            /* nasledujuce tri JQuery by mali handlovat aj drop file ale dnes mi nejak blbne chrome takze to neviem otestovat */
-            /* kludne zmaz ked ti to nepojde a nechces to */
-    
-            $(config.inputContainer).on(
-                'dragover',
-                function(e) {
-                    e.preventDefault();
-                    e.stopPropagation();
-                }
-            );
-    
-            $(config.inputContainer).on(
-                'dragenter',
-                function(e) {
-                    e.preventDefault();
-                    e.stopPropagation();
-                }
-            );
-    
-            $(config.inputContainer).on(
-                'drop',
-                function(e){
-                    if (e.originalEvent.dataTransfer && e.originalEvent.dataTransfer.files.length) {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        /*UPLOAD FILES HERE*/
-                        readFile(e.originalEvent.dataTransfer.files[0]);
-                    }
-                }
-            );
         }
-    
     </script>
 @endprepend
-
-@push('css')
-    <link @nonce href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.css" rel="stylesheet"> 
-    <style>
-        .preview-container > * {
-            margin: 20px auto;
-            /* height: 500px; */
-            max-height: 300px;
-        }
-        .preview-container img {
-            margin: auto;
-            object-fit: scale-down;
-            width: 100%;
-            margin-bottom: 15px;
-        }
-        .upload-area {
-            border: 5px dotted #dadada;
-            height: 100px;
-            width: 100%;
-            border-radius: 10px;
-            display: flex;
-            cursor: pointer;
-        }
-        .upload-area input {
-            margin: auto;
-        }
-        .crop-container {
-            display: flex;
-            justify-content: center;
-            padding: 0px;
-        }
-    </style>
-@endpush
