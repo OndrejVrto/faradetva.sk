dd7b822a9fb6daaf2a7c5d028c0834bbd48ba5fe
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-05-15 09:39:55 +0200

    add new pages template

---
+++ resources/views/web/custom/spolocenstva/barka.blade.php
@@ -0,0 +1,9 @@
+<x-web.layout.master :pageData="$pageData">
+
+    {{-- Bárka --}}
+
+    <x-web.page.section name="PAGE: ({{$pageData['title']}}) -" class="static-page pad_b_50">
+
+
+    </x-web.page.section>
+</x-web.layout.master>
---
+++ resources/views/web/custom/spolocenstva/modlitby-matiek.blade.php
@@ -0,0 +1,9 @@
+<x-web.layout.master :pageData="$pageData">
+
+    {{-- Modlitby matiek --}}
+
+    <x-web.page.section name="PAGE: ({{$pageData['title']}}) -" class="static-page pad_b_50">
+
+
+    </x-web.page.section>
+</x-web.layout.master>
