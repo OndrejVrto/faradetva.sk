143f9622813e896fe8965bdae9e4716066ec81cd
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-05-18 23:55:48 +0200

    Add pictures to pages "Poboznosti" and "Pohreb"

--- resources/views/web/custom/duchovny-zivot/sveteniny/poboznosti.blade.php
+++ resources/views/web/custom/duchovny-zivot/sveteniny/poboznosti.blade.php
@@ -2,11 +2,15 @@
 
     {{-- Pobožnosti --}}
 
-    <x-web.page.section name="PAGE: ({{$pageData['title']}}) -" class="static-page pad_b_50">
+    <x-web.page.section name="PAGE: ({{$pageData['title']}}) PART I -" class="static-page pad_b_50">
 
         <x-web.page.subsection>
-            {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4" /> --}}
+
             <x-web.page.text-segment animation="fromleft">
+
+                <x-partials.picture titleSlug="pob-005" side="left" animation="fromleft" dimensionSource="full" columns="3" />
+
+                <x-partials.picture titleSlug="pob-004" side="right" animation="fromright" dimensionSource="medium" columns="3" />
                 <p>
                     Pod pobožnosťami chápeme rozličné vonkajšie prejavy (napr. v textoch modlitieb a piesní, v zachovávaní období a navštevovania osobitných miest, v používaní odznakov, medailí, rúch a zvykov), ktoré oživené vnútorným postojom viery prejavujú zvláštny dôraz vzťahu veriaceho k božským osobám, alebo k Panne Márii v jej privilégiách milosti a v tituloch, ktoré ich vyjadrujú, alebo k svätým, pre ich pripodobnenie sa Kristovi, alebo pre úlohu, ktorú zohrali v Cirkvi.
                 </p>
@@ -22,21 +26,46 @@
         <x-web.page.subsection title="Liturgický rok a ľudová zbožnosť">
             {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4" /> --}}
             <x-web.page.text-segment>
+
+                <x-partials.picture titleSlug="pob-006" side="left" animation="fromleft" dimensionSource="medium" columns="3" />
+
                 <p>
                     Liturgický rok je časová štruktúra, v rámci ktorej Cirkev slávi celé Kristovo tajomstvo: od Vtelenia a Narodenia Pána až po Nanebovstúpenie, deň Zoslania Ducha Svätého a očakávanie blaženej nádeje a príchodu Pána. V liturgickom roku slávenie veľkonočného tajomstva tvorí privilegovaný moment kresťanského kultu v jeho každodennom, týždennom a ročnom vývoji. Z toho vyplýva, že vo vzťahu medzi liturgiou a ľudovou zbožnosťou sa má pokladať za pevný bod priorita slávení liturgického roka nad každým iným prejavom a úkonom zbožnosti.
                 </p>
+
+                <x-partials.picture titleSlug="pob-001" side="right" animation="fromright" dimensionSource="small" columns="2" />
+
                 <p>
                     V adventom období očakávania, obrátenia a nádeje patria medzi prejavy ľudovej zbožnosti: Adventný veniec, Zimné kántrové dni, Vianočná novéna - Kto dá prístrešie Svätej rodine? Vo vianočnom období Cirkev slávi tajomstvo Zjavenia Pána, jeho skromného narodenia v Betleheme. Ako je známe, okrem stavania betlehemských jasieľ v kostoloch, počnúc od 13. storočia sa rozšíril zvyk, ovplyvnený bezpochyby jasličkami pripravenými sv. Františkom z Assisi v roku 1223 v Grecciu, stavať malé betlehemy aj v domácich príbytkoch. Ľudovú zbožnosť vo vianočnom čase predstavujú: Betlehemy (aj „živé“), Vianočná (štedrovečerná) večera, Jasličkové pobožnosti, Koledovanie, Koncoročná pobožnosť, Trojkráľové požehnanie príbytkov.
                 </p>
                 <p>
                     Pôst je obdobie, ktoré predchádza a pripravuje na slávenie Veľkej noci. Je časom počúvania Božieho slova a obrátenia, zmierenia s Bohom a s bratmi, časom modlitby, pôstu a almužny. Začiatok štyridsaťdňového obdobia pokánia sa predstavuje jednoduchým symbolom „popolca“, ktorým sa vyznačuje Popolcová streda. Medzi pobožnosťami, ktorými si veriaci uctievajú Pánovo utrpenie, máloktoré sú také obľúbené ako Krížová cesta. Vo Veľkom týždni svätá Cirkev slávi tajomstvo spásy, zavŕšené Kristom v posledných dňoch jeho života. V tomto období je ľudová zbožnosť viditeľná v úkonoch ako: Návšteva miesta uloženia Najsvätejšej sviatosti na Zelený štvrtok, Predstavenie Kristovho utrpenia - pašiové hry, Lamentácie - Náreky proroka Jeremiáša, Návšteva „Božieho hrobu“. Vo veľkonočnom období sa vo viacerých oblastiach rozšírila pobožnosť Via lucis - cesta svetla, pri ktorej podobne ako pri krížovej ceste veriaci prechádzajú určitou cestou a uvažujú o rozličných zjaveniach Zmŕtvychvstalého Krista. V spojitosti s veľkonočnou oktávou sa rozšírila Pobožnosť k Božiemu milosrdenstvu, ktorá sa rozvinula aj do každodennej osobitnej modlitby Korunka k Božiemu milosrdenstvu. Pred slávnosťou Turíc - Zoslania Ducha Svätého je zaužívaná Novéna k Duchu Svätému.
                 </p>
+            </x-web.page.text-segment>
+        </x-web.page.subsection>
+
+        <x-partials.photo-gallery titleSlug="kalvaria-v-detve" dimensionSource="medium"/>
+
+    </x-web.page.section>
+
+    <x-web.sections.background-picture titleSlug="pob-002"/>
+
+    <x-web.page.section name="PAGE: ({{$pageData['title']}}) PART II -" class="static-page pad_b_50">
+
+        <x-web.page.subsection class="pt-5">
+            <x-web.page.text-segment>
+
                 <p>
                     V cezročnom období má po Nedeli Zoslania Ducha Svätého osobitné miesto Slávnosť Najsvätejšieho Kristovho tela a krvi. Typickými prvkami ľudovej zbožnosti počas slávnosti je výzdoba ulíc, kvety a oltáre, kde má byť pri zastaveniach procesie umiestnená Najsvätejšia sviatosť. Zvlášť rozšíreným prejavom eucharistického kultu je Adorácia Najsvätejšej sviatosti. V adorácii Najsvätejšej sviatosti sa zbližujú liturgické formy a prejavy ľudovej zbožnosti, a to v spôsoboch: a) jednoduchá návšteva Najsvätejšej sviatosti uloženej vo svätostánku kostola; b) adorácia pred vyloženou Oltárnou sviatosťou v monštrancii alebo v cibóriu; c) takzvaná „večná poklona“ alebo „celodenná poklona“, ktorej sa zúčastní celá rehoľná komunita alebo eucharistické združenie, prípadne farské spoločenstvo.
                 </p>
+
+                <x-partials.picture titleSlug="pob-008" side="left" animation="fromleft" dimensionSource="medium" columns="6" />
+
                 <p>
                     V Detve sa na Sviatok Božieho tela konala vždy procesia sprevádzaná dychovou hudbou. Na štyroch miestach v uliciach Detvy okolo kostola miestne ženy urobili zelené oltáriky - koľibki, pri ktorých sa konala bohoslužba. Prvá bola na dolnom konci pri feštrovi, druhá pred Lašouskím, treťa pri Floriánkovi a štvrtá pred Vagačovci. (Svedectvo ženy narodenej v r. 1929) Kňaz s hostiou v monštrancii kráčal pod baldachýnom, ktorý niesli štyria chlapi v krojoch. Mládenci niesli vyzdobené zástavy a z nich visiace stuhy niesli dievky oblečené v bielych krojoch. Malé dievčatá posýpali cestu pred kňazom kvetnými lupeňmi. Táto tradícia eucharistickej procesie v blízkosti kostola s ľudovými prvkami v sviatočných krojoch sa v Detve zachovala dodnes.
                 </p>
+                <x-partials.picture titleSlug="pob-017" side="right" animation="fromright" dimensionSource="full" columns="3" />
+
                 <p>
                     V našom prostredí je zaužívaná aj Prvonedeľná pobožnosť pred vyloženou Oltárnou sviatosťou po „veľkej“ nedeľnej svätej omši o 10.30 hod. V prvopiatkovom týždni býva vo štvrtok po večernej svätej omši o 17.30 hod. hodinová eucharistická adorácia a v piatok býva vo farskom kostole adorácia od 16.30 hod., ktorá je pred večernou svätou omšou ukončená Prvopiatkovou pobožnosťou. Táto pobožnosť pred vyloženou Oltárnou sviatosťou býva na prvý piatok mesiaca taktiež v kláštore bosých karmelitánok po rannej svätej omši o 7.00 hod. Počas celého kalendárneho roka býva v kláštornom kostole moderovaná adorácia Najsvätejšej sviatosti vždy v stredu od 19.30 do 20.30 hod.
                 </p>
@@ -49,9 +78,14 @@
             </x-web.page.text-segment>
         </x-web.page.subsection>
 
+        <x-partials.photo-gallery titleSlug="sviatok-bozieho-tela" dimensionSource="medium" class="pb-5"/>
+
         <x-web.page.subsection title="Uctievanie svätej Matky Pána Ježiša" animation="fromleft">
             {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4" /> --}}
             <x-web.page.text-segment animation="fromleft">
+
+                <x-partials.picture titleSlug="pob-020" side="left" animation="fromleft" dimensionSource="medium" columns="2" />
+
                 <p>
                     Ľudová zbožnosť k Preblahoslavenej Panne Márii, rozličná vo svojich prejavoch a hlboká vo svojom zdôvodnení, je významná a všeobecná cirkevná skutočnosť. Vyplýva z viery a lásky Božieho ľudu ku Kristovi, Vykupiteľovi ľudského pokolenia a z pochopenia spásonosného poslania, ktoré Boh zveril nazaretskej Márii, pre ktoré svätá Panna je nielen Matkou Pána a Spasiteľa, ale v pláne milosti aj Matkou všetkých ľudí.
                 </p>
@@ -67,9 +101,15 @@
         <x-web.page.subsection title="Uctievanie svätých a blahoslavených">
             {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4" /> --}}
             <x-web.page.text-segment>
+
+                <x-partials.picture titleSlug="pob-019" side="right" animation="fromright" dimensionSource="medium" columns="2" />
+
                 <p>
                     Uctievanie svätých a blahoslavenýchKult svätých, predovšetkým mučeníkov, je veľmi dávnou cirkevnou skutočnosťou. Cirkevná náuka a jej liturgia predkladajú svätých a blahoslavených, ktorí už jasne kontemplujú trojjediného Boha, za príklad Božiemu ľudu. Svätí sú historickí svedkovia všeobecného povolania k svätosti, významní Pánovi učeníci, vzory evanjeliového života, obyvatelia neba, orodovníci a priatelia ešte putujúcich na zemi, patróni miestnych cirkví.
                 </p>
+
+                <x-partials.picture titleSlug="pob-018" side="left" animation="fromleft" dimensionSource="medium" columns="3" />
+
                 <p>
                     Konečným cieľom úcty k svätým je vždy Božia sláva a posvätenie človeka prostredníctvom života dokonale zladeného s Božou vôľou a nasledovanie čností tých, ktorí boli vynikajúcimi Pánovými učeníkmi. Osobitnú úctu v prejavoch zbožnosti majú aj anjeli, v ľudovej zbožnosti zvlášť úcta k anjelovi strážcovi.
                 </p>
@@ -79,9 +119,18 @@
             </x-web.page.text-segment>
         </x-web.page.subsection>
 
+        <x-partials.photo-gallery titleSlug="relikvie-sv-terezie-z-lisieux" dimensionSource="medium"/>
+
         <x-web.page.information-sources title="Použitá literatúra:">
-            <li>OSTRIHOŇOVÁ, Anna. Kalendárne obyčaje a obyčaje životného cyklu. In: Anna Ostrihoňová. ed. Detva. Monografia mesta. Detva: Mesto Detva, 2018. ISBN 978-80-9730011-2-5.</li>
-            <li>Direktórium o ľudovej zbožnosti a liturgii. Trnava: Spolok sv. Vojtecha, 2005. ISBN 80-7162-547-7.</li>
+            <li>
+                OSTRIHOŇOVÁ, Anna. Kalendárne obyčaje a obyčaje životného cyklu. In: Anna Ostrihoňová. ed.
+                <em class="me-1">Detva. Monografia mesta.</em>
+                Detva: Mesto Detva, 2018. ISBN 978-80-9730011-2-5.
+            </li>
+            <li>
+                <em class="me-1">Direktórium o ľudovej zbožnosti a liturgii.</em>
+                Trnava: Spolok sv. Vojtecha, 2005. ISBN 80-7162-547-7.
+            </li>
         </x-web.page.information-sources>
 
     </x-web.page.section>
--- resources/views/web/custom/duchovny-zivot/sveteniny/pohreb.blade.php
+++ resources/views/web/custom/duchovny-zivot/sveteniny/pohreb.blade.php
@@ -5,26 +5,30 @@
     <x-web.page.section name="PAGE: ({{$pageData['title']}}) -" class="static-page pad_b_50">
 
     <x-web.page.subsection >
-        {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4"/> --}}
+
         <x-web.page.text-segment animation="fromright">
+
+            <x-partials.picture titleSlug="pohr-002" side="left" animation="fromleft" dimensionSource="full" columns="3" />
+
             <p>
                 Cirkev sa modlí a verí: „Očakávam vzkriesenie mŕtvych a život budúceho veku.“ Pohrebné obrady sú posvätným úkonom, pri ktorom Cirkev zveruje Bohu svojich bratov a sestry vo viere, vyznáva svoju vieru a uznáva Božie pôsobenie v živote človeka.
             </p>
             <p>
                 Utrpenie, smrť a zmŕtvychvstanie Ježiša Krista sú vzorom života kresťana. Obradmi, ktoré sprevádzajú rozlúčku so zosnulými, spoločenstvo veriacich slávi nielen spásu, ktorú pre nás získal Kristus, ale nás aj zjednocuje s príbuznými, priateľmi a známymi, s ktorými zosnulí bratia a sestry prežívali časť svojho života, práce, vzťahov, citov a viery v Boha.
             </p>
+
+            <x-partials.picture titleSlug="pohr-001" side="right" animation="fromright" dimensionSource="full" columns="3" />
+
             <p>
                 Kresťanská liturgia pohrebu je slávením Kristovho veľkonočného tajomstva, pri ktorom sa Cirkev modlí za svoje deti, začlenené sviatosťou krstu do smrti a zmŕtvychvstania Ježiša Krista. Preto Cirkev, ako súcitná matka, svojimi modlitbami a prinášaním obety svätej omše chce byť pre zomrelých duchovnou pomocou a pre pozostalých útechou i nádejou v ich žiali, že všetci pokrstení v Krista raz vstanú k novému životu.
             </p>
-        </x-web.page.text-segment>
-    </x-web.page.subsection>
 
-    <x-web.page.subsection>
-        {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4" /> --}}
-        <x-web.page.text-segment animation="fromleft">
             <p>
                 Pri pohrebe svojich veriacich Cirkev s dôverou slávi veľkonočné tajomstvo, aby tí, čo boli krstom pričlenení k zomrelému a vzkriesenému Kristovi, smrťou vošli s ním do života. Ich duše sa majú očistiť a prejsť do spoločenstva svätých a vyvolených v nebi, ich telá očakávajú blaženú nádej Kristovho príchodu a vzkriesenie mŕtvych. Preto Cirkev prináša za zomrelých veľkonočnú eucharistickú Kristovu obetu a koná za nich prosebné modlitby. Chce zomrelým vyprosiť duchovnú pomoc a živým poskytnúť útechu nádeje. Veď všetky údy tajomného Kristovho tela tvoria jeho spoločenstvo.
             </p>
+
+            <x-partials.picture titleSlug="pohr-004" side="left" animation="fromleft" dimensionSource="full" columns="4" />
+
             <p>
                 Pri pohrebe svojich bratov a sestier majú kresťania vyjadriť svoju pevnú nádej na večný život. Zároveň však majú dbať na zmýšľanie svojich súčasníkov a na smútočné zvyky svojej krajiny. Čo by však odporovalo evanjeliu, nech sa usilujú zmeniť, aby sa pri kresťanskom pohrebe prejavovala veľkonočná viera a skutočný duch evanjelia. Zaiste treba odmietnuť prázdnu, vonkajšiu honosnosť pohrebu, patrí sa však preukazovať úctu telám zomrelých veriacich, veď boli chrámom Ducha Svätého.
             </p>
@@ -37,12 +41,19 @@
     <x-web.page.subsection title="Pochovávanie a uchovávanie popola v prípade kremácie">
         {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4"/> --}}
         <x-web.page.text-segment animation="fromright">
+
+            <x-partials.picture titleSlug="pohr-005" side="left" animation="fromleft" dimensionSource="full" columns="3" />
+
             <p>
                 Aby sme mohli vstať s Kristom z mŕtvych, musíme zomrieť s Kristom, „vzdialiť sa z tela a bývať u Pána“ (2Kor 5,8). Inštrukciou Piam et constantem z 5. júla 1963, Sväté ofícium ustanovilo, aby sa „verne zachovával zvyk pochovávať mŕtve telá veriacich“, pričom sa dodáva, že kremácia „neprotirečí kresťanskému náboženstvu“ a už sa nemajú odopierať sviatosti a pohrebné obrady tým, ktorí si želali byť spopolnení. Platí to však za podmienky, že ich želanie nie je motivované „popieraním kresťanských dogiem, alebo sektárskym zmýšľaním, alebo nenávisťou voči katolíckemu náboženstvu a Cirkvi“.
             </p>
+
+            <x-partials.picture titleSlug="pohr-006" side="right" animation="fromright" dimensionSource="full" columns="3" />
+
             <p>
                 Kristovo zmŕtvychvstanie, ktoré predstavuje vrcholnú pravdu kresťanskej viery, sa od počiatkov kresťanstva hlása ako podstatná súčasť veľkonočného tajomstva. Prostredníctvom svojej smrti a zmŕtvychvstania nás Kristus oslobodil od hriechu a umožnil nám vstúpiť do nového života: „... aby sme tak, ako bol Kristus vzkriesený z mŕtvych Otcovou slávou, aj my žili novým životom“ (Rim 6,4). Okrem toho je zmŕtvychvstalý Kristus počiatkom a zdrojom nášho budúceho života: „Kristus vstal z mŕtvych, prvotina zosnulých... ako všetci umierajú v Adamovi, tak zasa všetci ožijú v Kristovi.“ (1Kor 15,20-22).
             </p>
+
             <p>
                 Vďaka Kristovi má kresťanská smrť pozitívny zmysel. Liturgia Cirkvi sa modlí: „Veď tým, čo veria v teba, Bože, život sa neodníma iba mení; a keď skončíme život v smrteľnom tele, máme pripravený večný príbytok v nebesiach.“ Pri smrti sa duša oddelí od tela, no pri vzkriesení Boh dáva neporušiteľný život nášmu premenenému telu, ktoré spojí s našou dušou. Aj dnes je Cirkev povolaná ohlasovať vieru vo vzkriesenie: „Vzkriesenie mŕtvych je vierou kresťanov: stali sme sa nimi vierou v toto vzkriesenie.“
             </p>
@@ -55,6 +66,9 @@
             <p>
                 Nasledujúc najstaršiu kresťanskú tradíciu Cirkev neúnavne odporúča, aby boli telá zosnulých pochované na cintoríne alebo na inom posvätnom mieste. Pri pamiatke na Pánovu smrť, pochovanie do hrobu a vzkriesenie, teda tajomstvo, vo svetle ktorého sa vyjavuje kresťanský zmysel smrti, je pochovanie tela tou najvhodnejšou formou na vyjadrenie viery a nádeje vo vzkriesenie tela. Cirkev, ktorá ako matka sprevádza kresťana počas jeho pozemskej púte, v Kristovi obetuje Otcovi dieťa jeho milosti a s nádejou, že ho vzkriesi v sláve, ukladá jeho telesné pozostatky do zeme.
             </p>
+
+            <x-partials.picture titleSlug="pohr-007" side="right" animation="fromright" dimensionSource="full" columns="4" />
+
             <p>
                 Keď Cirkev pochováva telá zosnulých, potvrdzuje tým vieru vo vzkriesenie tela a chce zdôrazniť vysokú dôstojnosť ľudského tela ako integrálnej súčasti ľudskej osoby, s ktorou má telo spoločnú históriu. Preto nie sú prípustné také postoje a obrady, ktoré by zahŕňali mylné predstavy o smrti, považovanej či už za definitívne zničenie osoby, za moment splynutia s matkou prírodou alebo s vesmírom, alebo za istú etapu v procese reinkarnácie či definitívne oslobodenie z „väzenia“ tela.
             </p>
@@ -64,12 +78,9 @@
             <p>
                 Pochovávaním tiel na cintorínoch, v kostoloch a na pozemkoch k nim prináležiacich, uchovávala kresťanská tradícia spoločenstvo medzi živými a zosnulými a stavala sa proti tendencii utajovať smrť a jej význam pre kresťanov, či považovať ju za privátnu udalosť.
             </p>
-        </x-web.page.text-segment>
-    </x-web.page.subsection>
 
-    <x-web.page.subsection>
-        {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4"/> --}}
-        <x-web.page.text-segment animation="fromright">
+            <x-partials.picture titleSlug="pohr-008" side="left" animation="fromleft" dimensionSource="full" columns="3" />
+
             <p>
                 Tam, kde sa z hygienických, ekonomických alebo sociálnych dôvodov rozhodne pre kremáciu, ak toto rozhodnutie nie je proti výslovnému alebo oprávnene predpokladanému želaniu zosnulého veriaceho, nevidí Cirkev doktrinálne dôvody, aby sa takejto praxi bránilo, pretože kremácia tela sa nedotýka duše a nebráni Božej všemohúcnosti, aby vzkriesila telo, a teda neobsahuje objektívne popieranie kresťanskej náuky o nesmrteľnosti duše a vzkriesení tela.
             </p>
@@ -85,9 +96,15 @@
     <x-web.page.subsection>
         {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4" /> --}}
         <x-web.page.text-segment animation="fromleft">
+
+            <x-partials.picture titleSlug="pohr-009" side="right" animation="fromright" dimensionSource="full" columns="3" />
+
             <p>
                 Uchovávanie popola na posvätnom mieste môže prispieť k zmenšeniu rizika, že sa zosnulí vytratia z modlitieb a spomienok svojich príbuzných a kresťanského spoločenstva. Bráni to tiež zabúdaniu a nedostatku úcty - čo sa môže stať najmä vtedy, ak sa prvá generácia pominie - ale aj neprístojným praktikám či poverčivosti. Z vyššie uvedených dôvodov sa nedovoľuje uchovávať popol v domácnosti. Popol sa nesmie rozdeliť medzi rôznych členov rodiny a musí byť vždy zaistená náležitá úcta a primerané podmienky uchovávania.
             </p>
+
+            <x-partials.picture titleSlug="pohr-010" side="left" animation="fromleft" dimensionSource="full" columns="3" />
+
             <p>
                 Aby sa vyhlo akémukoľvek dvojznačnému panteizmu, naturalizmu alebo nihilizmu, nedovoľuje sa rozsypať popol do vzduchu, zeme alebo vody, či iným spôsobom, ani premeniť popol na upomienkové predmety, kusy bižutérie alebo iné predmety. V súvislosti s takýmto konaním totiž nemožno uznať, že rozhodnutie pre kremáciu bolo motivované hygienickými, sociálnymi či ekonomickými dôvodmi.
             </p>
@@ -97,8 +114,7 @@
         </x-web.page.text-segment>
     </x-web.page.subsection>
 
-    <x-web.page.subsection title="Ako vybaviť pohreb">
-        {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4"/> --}}
+    {{-- <x-web.page.subsection title="Ako vybaviť pohreb">
         <x-web.page.text-segment animation="fromright">
             <p>
                 Pohreb blízkej osoby sa vybavuje v spolupráci pohrebnej služby, farského i mestského úradu. V prípade úmrtia doma je potrebné bezodkladne kontaktovať pohotovosť (t. č. 112) a oznámiť úmrtie. Počkať na obhliadajúceho lekára, ktorý vystaví List o prehliadke mŕtveho a následne dohodnúť odvoz zomrelého s pohrebnou službou. V prípade úmrtia v nemocnici, v domove dôchodcov alebo v iných zariadeniach sociálnych služieb, po oznámení úmrtia kontaktovať pohrebnú službu kvôli odvozu zomrelého. Po dohode cirkevného pohrebu v našej farnosti sa úmrtie oznamuje verejnosti zvonením na kostolnej veži o 10.00 a 16.00 hod., každý deň až do konania pohrebu.
@@ -110,11 +126,20 @@
                 Zomrelí, ktorí boli v poslednom období pred úmrtím zaopatrení sviatosťami Cirkvi (svätá spoveď, sväté prijímanie alebo pomazanie chorých), môžu byť pochovaní aj so svätou omšou. Farnosť Detva pri pohrebných obradoch zabezpečuje okrem kňaza taktiež organistu. Keď pohrebný obrad nie je spojený so svätou omšou, odporúča sa dať za zomrelého následne odslúžiť svätú omšu v príhodnom čase. Je vhodné, aby veriaci na hroby svojich príbuzných umiestňovali symboly zmŕtvychvstania a večného života, ako aj nápisy vzaté zo Svätého písma a starokresťanskej tradície. Je potrebné dbať o to, aby cintoríny boli svedectvom kresťanskej viery v zmŕtvychvstanie tela.
             </p>
         </x-web.page.text-segment>
-    </x-web.page.subsection>
+    </x-web.page.subsection> --}}
 
     <x-web.page.information-sources title="Použitá literatúra:">
-        <li>Pohrebné obrady. Trnava: Spolok sv. Vojtecha, 2008. ISBN 978-80-7162-710-4.</li>
-        <li>Inštrukcia „Ad resurgendum cum Christo“ ohľadom pochovávania zomrelých a uchovávania popola v prípade kremácie. [online]. [cit. 26.01.2022]. Dostupné na internete: https://www.kbs.sk/obsah/sekcia/h/dokumenty-a-vyhlasenia/p/dokumenty-vatikanskych-uradov/c/instrukcia-ad-resurgendum-cum-christo</li>
+        <li>
+            <em>Pohrebné obrady.</em> Trnava: Spolok sv. Vojtecha, 2008. ISBN 978-80-7162-710-4.
+        </li>
+        <li>
+            <em class="me-1">Inštrukcia „Ad resurgendum cum Christo“ ohľadom pochovávania zomrelých a uchovávania popola v prípade kremácie.</em>
+            [online]. [cit. 26.01.2022].
+            <a class="link-template"
+            href="https://www.kbs.sk/obsah/sekcia/h/dokumenty-a-vyhlasenia/p/dokumenty-vatikanskych-uradov/c/instrukcia-ad-resurgendum-cum-christo">
+            Dostupné na internete
+        </a>
+        </li>
     </x-web.page.information-sources>
 
     </x-web.page.section>
