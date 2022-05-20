b0ea69aa9278b206090a8ee215c130ef9d7d4c12
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-05-20 10:02:40 +0200

    refactor card-event - remove duplicate picture

--- resources/views/components/partials/cart-event/index.blade.php
+++ resources/views/components/partials/cart-event/index.blade.php
@@ -4,20 +4,30 @@
     'url'   => null,
     'img'   => null,
 ])
-<div class="event_box from{{ $side }} wow">
 
-    @if ($side === 'right')
-        <div class="d-lg-none event_thumb">
-            {!! $img !!}
-        </div>
-    @else
-        <div class="event_thumb">
-            {!! $img !!}
-        </div>
-    @endif
+<div @class([
+        'event_box d-lg-flex wow',
+        'fromright' => $side === 'right',
+        'fromleft'  => $side !== 'right',
+    ])
+>
 
-    <div class="event_desc">
-        <h3>{{ $title }}</h3>
+    <div @class([
+            'event_thumb',
+            'order-2' => $side === 'right',
+        ])
+    >
+        {!! $img !!}
+    </div>
+
+    <div @class([
+            'event_desc',
+            'order-1' => $side === 'right',
+        ])
+    >
+        <h3>
+            {{ $title }}
+        </h3>
         <p class="d-xl-none">
             {{ $small_teaser }}
         </p>
@@ -27,14 +37,17 @@
         <p class="d-none d-xxl-block">
             {{ $full_teaser }}
         </p>
-        <a href="{{ $url }}" class="event_btn read_btn text-justify">
-            Dozvedieť sa viac
-        </a>
+        <div
+            @class([
+                'd-lg-flex',
+                'justify-content-start' => $side === 'right',
+                'justify-content-end' => $side !== 'right',
+            ])
+        >
+            <a href="{{ $url }}" class="event_btn read_btn">
+                Dozvedieť sa viac
+            </a>
+        </div>
     </div>
 
-    @if ($side === 'right')
-        <div class="event_thumb d-none d-lg-table-cell">
-            {!! $img !!}
-        </div>
-    @endif
 </div>
