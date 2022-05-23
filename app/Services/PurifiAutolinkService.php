e98c6197e3891c568a46ec824da7a6a0adaf072f
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-05-23 21:27:52 +0200

    Add null check to Purify URL Service

--- app/Services/PurifiAutolinkService.php
+++ app/Services/PurifiAutolinkService.php
@@ -8,17 +8,19 @@ use OsiemSiedem\Autolink\Elements\UrlElement;
 
 class PurifiAutolinkService
 {
-    public function getCleanTextWithLinks(string $text, string $class = 'link-template') {
-        return Purify::clean(
-            Autolink::convert( $text, function($element) use($class) {
-                return new UrlElement(
-                    title: $element->getTitle(),
-                    url: $element->getUrl(),
-                    start: $element->getStart(),
-                    end: $element->getEnd(),
-                    attributes: ['class' => $class],
-                );
-            }
-        ));
+    public function getCleanTextWithLinks(?string $text, string $class = 'link-template'): ?string {
+        return !$text
+            ? null
+            : Purify::clean(
+                Autolink::convert( $text, function($element) use($class) {
+                    return new UrlElement(
+                        title: $element->getTitle(),
+                        url: $element->getUrl(),
+                        start: $element->getStart(),
+                        end: $element->getEnd(),
+                        attributes: ['class' => $class],
+                    );
+                }
+            ));
     }
 }
