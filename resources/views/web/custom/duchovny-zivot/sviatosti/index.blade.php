6b5868a254844afb54e210676bf9e558a7de0b02
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-05-24 21:52:02 +0200

    add picture to pages "Sviatosti"

--- resources/views/web/custom/duchovny-zivot/sviatosti/index.blade.php
+++ resources/views/web/custom/duchovny-zivot/sviatosti/index.blade.php
@@ -2,46 +2,15 @@
 
     {{-- Duchovný život / Sviatosti --}}
 
-    <x-web.page.section name="PAGE: ({{$pageData['title']}}) PART 1 -" row="true" class="ch_blog_section pad_b_50">
+    <x-web.page.section name="PAGE: ({{$pageData['title']}}) PART 1 -" row="true" class="pad_t_30 pad_b_30">
 
-        <x-partials.card-article-small
-            title="Krst"
-            url="{{ secure_url('duchovny-zivot/sviatosti/krst') }}"
-            delay="1"
-        >
-            <x-slot:img>
-                <x-partials.picture-responsive titleSlug="pob-002-menu"/>
-            </x-slot:img>
-            <x-slot:teaser>
-                Krst zjednocuje s Ježišom Kristom, vťahuje do jeho vykupiteľskej smrti na kríži, a tak vyslobodzuje z moci hriechu, očisťuje od všetkých osobných hriechov a umožňuje spolu s ním vstať z mŕtvych k životu, ktorý sa nekončí.
-            </x-slot:teaser>
-        </x-partials.card-article-small>
-
-        <x-partials.card-article-small
-            title="Birmovanie"
-            url="{{ secure_url('duchovny-zivot/sviatosti/birmovanie') }}"
-            delay="2"
-        >
-            <x-slot:img>
-                <x-partials.picture-responsive titleSlug="pob-002-menu"/>
-            </x-slot:img>
-            <x-slot:teaser>
-                Kto sa v slobode rozhodne žiť ako Božie dieťa a kto požiada o Ducha Svätého prostredníctvom vkladania rúk a pomazania krizmou, dostane silu svedčiť o Božej láske slovom i skutkom.
-            </x-slot:teaser>
-        </x-partials.card-article-small>
-
-        <x-partials.card-article-small
-            title="Eucharistia"
-            url="{{ secure_url('duchovny-zivot/sviatosti/eucharistia') }}"
-            delay="3"
-        >
-            <x-slot:img>
-                <x-partials.picture-responsive titleSlug="pob-002-menu"/>
-            </x-slot:img>
-            <x-slot:teaser>
-                Eucharistia je tajomným stredobodom iniciačných sviatostí, pretože Ježišova historická obeta na kríži sa pri premenení sprítomňuje skrytým nekrvavým spôsobom. Tak sa slávenie Eucharistie stáva „prameňom a vrcholom celého kresťanského života“.
-            </x-slot:teaser>
-        </x-partials.card-article-small>
+        <x-partials.page-card
+            routeStaticPages="
+                duchovny-zivot.sviatosti.krst,
+                duchovny-zivot.sviatosti.birmovanie,
+                duchovny-zivot.sviatosti.eucharistia,
+            "
+        />
 
     </x-web.page.section>
 
@@ -53,11 +22,18 @@
             side="left"
         >
             <x-slot:img>
-                <x-partials.picture-responsive titleSlug="pob-002-menu"/>
+                <x-partials.picture-responsive titleSlug="sacr4-001-menu"/>
             </x-slot:img>
             <x-slot:teaser>
-                {{-- TODO:  --}}
-                Krst nás síce oslobodzuje z moci hriechu a smrti a uvádza nás do nového života Božích detí, ale neoslobodzuje nás od ľudskej slabosti a od náklonnosti k hriechu. Preto sa potrebujeme znovu a znovu uzmierovať s Bohom. Túto možnosť nám poskytuje sviatosť zmierenia.
+                Sviatosť zmierenia sa nazýva tiež sviatosťou pokánia, odpustenia, obrátenia alebo spoveďou.
+                Krst nás síce oslobodzuje z moci hriechu a smrti a uvádza nás do nového života Božích detí, ale neoslobodzuje nás od ľudskej slabosti a od náklonnosti k hriechu.
+                Preto sa potrebujeme znovu a znovu uzmierovať s Bohom.
+                Túto možnosť nám poskytuje sviatosť zmierenia.
+                <span class="d-none d-xl-inline">
+                    Nikde nie je krajšie vysvetlená podstata sviatosti zmierenia ako v podobenstve o milosrdnom otcovi.
+                    Blúdime a strácame smer, ale náš Otec na nás čaká s veľkou, ba nekonečnou túžbou.
+                    Odpúšťa nám, keď sa k nemu navraciame.
+                </span>
             </x-slot:teaser>
         </x-partials.cart-event>
 
@@ -67,11 +43,16 @@
             side="right"
         >
             <x-slot:img>
-                <x-partials.picture-responsive titleSlug="pob-002-menu"/>
+                <x-partials.picture-responsive titleSlug="sacr5-001-menu"/>
             </x-slot:img>
             <x-slot:teaser>
-                {{-- TODO:  --}}
-                Podstatou obradu pri slávení sviatosti pomazania chorých je modlitba kňaza spojená s pomazaním čela a rúk chorého posväteným olejom. Ježiš Kristus mocou vysvätenia biskupa alebo kňaza koná prostredníctvom nich.
+                Túto sviatosť môže prijať každý pokrstený, ktorý sa nachádza v kritickom zdravotnom stave alebo situácii ohrozujúcej život.
+                Sviatosť pomazania chorých môžeme prijať v živote viackrát.
+                Preto má zmysel požiadať o túto sviatosť aj v mladom veku, ak má človek podstúpiť ťažkú operáciu.
+                <span class="d-none d-xl-inline">
+                    Mnohí kresťania v takejto situácii spájajú sviatosť pomazania chorých so sviatosťou zmierenia, spolu so svätým prijímaním; v každom prípade chcú predstúpiť pred Boha s čistým svedomím.
+                    Podstatou obradu slávenia tejto sviatosti je modlitba kňaza spojená s pomazaním čela a rúk posväteným olejom.
+                </span>
             </x-slot:teaser>
         </x-partials.cart-event>
 
@@ -85,10 +66,17 @@
             url="{{ secure_url('duchovny-zivot/sviatosti/posvatny-stav') }}"
         >
             <x-slot:img>
-                <x-partials.picture-responsive titleSlug="pob-002-menu"/>
+                <x-partials.picture-responsive titleSlug="sacr6-003"/>
             </x-slot:img>
             <x-slot:teaser>
-                Vysviackou dostáva kňaz konkrétnu moc a poslanie slúžiť svojim bratom a sestrám vo viere. Sviatosť posväteného stavu má tri stupne: biskupský stav (episkopát), kňazský stav (presbyterát), diakonský stav (diakonát).
+                Ten, kto prijíma sviatosť posvätného stavu, dostáva osobitnú účasť na Kristovom kňazstve, ktoré sa kvalitatívne líši od všeobecného kňazstva veriacich.
+                Sviatostné kňazstvo je dar Ducha Svätého, ktorý udeľuje Kristus prostredníctvom Cirkvi.
+                Zo sviatostného kňazstva nevyplýva úloha plniť iba určitú funkciu alebo zastávať nejaký úrad.
+                Vysviackou dostáva kňaz konkrétnu moc a poslanie slúžiť svojim bratom a sestrám vo viere.
+                <span class="d-none d-xl-inline">
+                    Sviatosť posväteného stavu má tri stupne: biskupský stav (episkopát), kňazský stav (presbyterát), diakonský stav (diakonát).
+                    Za diakona, kňaza a biskupa môže byť platne vysvätený muž, ktorý prijal katolícku vieru, bol v nej pokrstený a pobirmovaný a je Cirkvou povolaný na tento úrad.
+                </span>
             </x-slot:teaser>
         </x-partials.card-about>
 
@@ -98,10 +86,15 @@
             url="{{ secure_url('duchovny-zivot/sviatosti/manzelstvo') }}"
         >
             <x-slot:img>
-                <x-partials.picture-responsive titleSlug="pob-002-menu"/>
+                <x-partials.picture-responsive titleSlug="sacr7-003-menu"/>
             </x-slot:img>
             <x-slot:teaser>
-                Manželstvo sa môže uzavrieť iba vtedy, ak jestvuje manželský konsenzus, to znamená, že muž a žena chcú vstúpiť do manželstva slobodne, bez strachu alebo nátlaku a keď im v tom nebránia nijaké prirodzené alebo cirkevné záväzky.
+                Sviatosť manželstva vychádza zo sľubu muža a ženy pred Bohom a pred Cirkvou, ktorý prijíma a potvrdzuje Boh a napĺňa sa fyzickým zjednotením oboch partnerov.
+                Pretože Boh sám spečaťuje puto sviatostného manželstva, zaväzuje tento zväzok až do smrti jedného z partnerov.
+                <span class="d-none d-xl-inline">
+                    Manželstvo sa môže uzavrieť iba vtedy, ak jestvuje manželský konsenzus, to znamená, že muž a žena chcú vstúpiť do manželstva slobodne, bez strachu alebo nátlaku a keď im v tom nebránia nijaké prirodzené alebo cirkevné záväzky (napr. už existujúce manželstvo alebo sľub celibátu).
+                </span>
+                K sviatostnému manželstvu nevyhnutne patria prvky: slobodný súhlas, dobrovoľné prijatie celoživotného výlučného zväzku, otvorenosť na prijatie potomstva.
             </x-slot:teaser>
         </x-partials.card-about>
 
--- resources/views/web/custom/duchovny-zivot/sviatosti/posvatny-stav.blade.php
+++ resources/views/web/custom/duchovny-zivot/sviatosti/posvatny-stav.blade.php
@@ -2,86 +2,100 @@
 
     {{-- Pozvätný stav --}}
 
-    <x-web.page.section name="PAGE: ({{$pageData['title']}}) -" class="static-page pad_b_50">
+    <x-web.page.section name="PAGE: ({{$pageData['title']}}) PART I -" class="static-page pad_b_50">
 
-    <x-web.page.subsection title="Čo je sviatosť posvätného stavu?">
-        {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4"/> --}}
-        <x-web.page.text-segment animation="fromright">
-            <p>
-                Ten, kto prijíma sviatosť posvätného stavu, dostáva osobitnú účasť na Kristovom kňazstve, ktoré sa kvalitatívne líši od všeobecného kňazstva veriacich. Sviatostné kňazstvo je dar Ducha Svätého, ktorý udeľuje Kristus prostredníctvom Cirkvi. Zo sviatostného kňazstva nevyplýva úloha plniť iba určitú funkciu alebo zastávať nejaký úrad. Vysviackou dostáva kňaz konkrétnu moc a poslanie slúžiť svojim bratom a sestrám vo viere. Sviatosť posväteného stavu má tri stupne: biskupský stav (episkopát), kňazský stav (presbyterát), diakonský stav (diakonát).
-            </p>
-        </x-web.page.text-segment>
-    </x-web.page.subsection>
+        <x-web.page.subsection title="Čo je sviatosť posvätného stavu?">
+            <x-web.page.text-segment animation="fromright">
 
-    <x-web.page.subsection title="Aké miesto má sviatosť posvätného stavu v Božom pláne spásy?">
-        {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="5" /> --}}
-        <x-web.page.text-segment animation="fromleft">
-            <p>
-                Kňazi Starého zákona boli predovšetkým prostredníkmi medzi nebo a zemou, medzi Bohom a jeho národom. Ježiš Kristus ako jediný „prostredník medzi Bohom a ľuďmi“ (1Tim 2,5) toto starozákonné kňazstvo naplnil, dovŕšil a ukončil. Po Ježišovej vykupiteľskej obete môže existovať už iba sviatostné kňazstvo v Kristovi, v Kristovej obete na kríži a skrze Kristovo povolanie a apoštolské poslanie. Katolícky kňaz, ktorý udeľuje sviatosti, nekoná na vlastnú päsť, z vlastnej moci alebo zo svojej morálnej dokonalosti, ale „in persona Christi“ - v osobe Krista. Vysviackou v ňom rastie Kristova premieňajúca, uzdravujúca a zachraňujúca moc, ktorou slúži zverenému ľudu.
-            </p>
-        </x-web.page.text-segment>
-    </x-web.page.subsection>
+                <x-partials.picture titleSlug="sacr6-004" side="right" dimensionSource="medium" columns="4" />
 
-    <x-web.page.subsection title="Kto môže prijať sviatosť posvätného stavu?">
-        <x-web.page.text-segment animation="fromright">
-            <p>
-                Za diakona, kňaza a biskupa môže byť platne vysvätený muž, ktorý prijal katolícku vieru, bol v nej pokrstený a pobirmovaný a je Cirkvou povolaný na tento úrad.
-            </p>
-        </x-web.page.text-segment>
-    </x-web.page.subsection>
+                <p>
+                    Ten, kto prijíma sviatosť posvätného stavu, dostáva osobitnú účasť na Kristovom kňazstve, ktoré sa kvalitatívne líši od všeobecného kňazstva veriacich. Sviatostné kňazstvo je dar Ducha Svätého, ktorý udeľuje Kristus prostredníctvom Cirkvi. Zo sviatostného kňazstva nevyplýva úloha plniť iba určitú funkciu alebo zastávať nejaký úrad. Vysviackou dostáva kňaz konkrétnu moc a poslanie slúžiť svojim bratom a sestrám vo viere. Sviatosť posväteného stavu má tri stupne: biskupský stav (episkopát), kňazský stav (presbyterát), diakonský stav (diakonát).
+                </p>
 
-    <x-web.page.subsection title="Aký účinok má diakonská vysviacka?">
-        {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="5" /> --}}
-        <x-web.page.text-segment animation="fromleft">
-            <p>
-                Diakon je ustanovený pre osobitnú službu Cirkvi. Zastupuje totiž Krista, ktorý „neprišiel dať sa obsluhovať, ale slúžiť a položiť svoj život ako výkupné za mnohých“ (Mt 20,28). V liturgii diakonskej vysviacky sa hovorí: „Posilnený darom Ducha Svätého bude pomáhať biskupovi a jeho kňazom v službe slova, oltára a charity. Stane sa služobníkom všetkých.“ Prvotným vzorom diakona je svätý mučeník Štefan, ktorý bol medzi prvými siedmimi diakonmi, ktorých ustanovili apoštoli, keď sa v službe prvotnej cirkvi v Jeruzaleme cítili preťažení množstvom charitatívnych úloh. Neskôr sa diakonská služba stala na dlhé storočia len nižším stupňom na ceste ku kňazstvu. Dnes sú opäť do tejto služby povolaní jednak tí, ktorí žijú v celibáte, ako aj ženatí muži. Takto chápaná diakonská služba nanovo zdôrazňuje služobný charakter Cirkvi. Zároveň poskytuje kňazom pomoc v duchu prvotnej Cirkvi tým, že diakon prijíma predovšetkým pastoračné a sociálne úlohy v Cirkvi. Aj diakonská vysviacka vtláča celoživotnú a nezrušiteľnú pečať.
-            </p>
-        </x-web.page.text-segment>
-    </x-web.page.subsection>
+                <h3 class="mt-2 wow fromright" data-wow-delay="0.4s">
+                    Aké miesto má sviatosť posvätného stavu v Božom pláne spásy?
+                </h3>
 
-    <x-web.page.subsection title="Aký účinok má kňazská vysviacka?">
-        {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4"/> --}}
-        <x-web.page.text-segment animation="fromright">
-            <p>
-                Pri kňazskej vysviacke Duch Svätý vtláča kňazovi nezmazateľnú pečať, ktorou ho pripodobňuje Kristovi a dáva mu schopnosť konať v Ježišovom mene. Kňaz ako spolupracovník svojho biskupa má hlásať Božie slovo, udeľovať sviatosti a predovšetkým sláviť Eucharistiu.
-            </p>
-        </x-web.page.text-segment>
-    </x-web.page.subsection>
+                <p>
+                    Kňazi Starého zákona boli predovšetkým prostredníkmi medzi nebo a zemou, medzi Bohom a jeho národom. Ježiš Kristus ako jediný „prostredník medzi Bohom a ľuďmi“ (1Tim 2,5) toto starozákonné kňazstvo naplnil, dovŕšil a ukončil. Po Ježišovej vykupiteľskej obete môže existovať už iba sviatostné kňazstvo v Kristovi, v Kristovej obete na kríži a skrze Kristovo povolanie a apoštolské poslanie. Katolícky kňaz, ktorý udeľuje sviatosti, nekoná na vlastnú päsť, z vlastnej moci alebo zo svojej morálnej dokonalosti, ale „in persona Christi“ - v osobe Krista. Vysviackou v ňom rastie Kristova premieňajúca, uzdravujúca a zachraňujúca moc, ktorou slúži zverenému ľudu.
+                </p>
 
-    <x-web.page.subsection title="Aký účinok má biskupská vysviacka?">
-        {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4" /> --}}
-        <x-web.page.text-segment animation="fromleft">
-            <p>
-                Biskupskou vysviackou sa udeľuje plnosť sviatosti posvätného stavu. Po vysvätení sa biskup stáva právoplatným nástupcom apoštolov a vstupuje do biskupského zboru. Od okamihu svojho vysvätenia je zodpovedný za celú Cirkev spolu s ostatnými biskupmi a s pápežom. Biskupskou vysviackou sa mu udeľuje poslanie učiť, posväcovať a spravovať. Biskup, ktorý spolu s kňazmi a diakonmi ako so svojimi vysvätenými spolupracovníkmi vykonáva pastiersku službu, je viditeľným princípom a základom miestnej cirkvi (biskupstva/diecézy).
-            </p>
-        </x-web.page.text-segment>
-    </x-web.page.subsection>
+                <h3 class="mt-2 wow fromright" data-wow-delay="0.4s">
+                    Kto môže prijať sviatosť posvätného stavu?
+                </h3>
 
-    <x-web.page.subsection title="Ako sa odlišuje všeobecné kňazstvo všetkých veriacich od sviatostného kňazstva?">
-        {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4"/> --}}
-        <x-web.page.text-segment animation="fromright">
-            <p>
-                Každý kresťan je povolaný k všeobecnému kňazstvu, aby v Božom mene pôsobil vo svete a prinášal mu Božie požehnanie a milosť. Pri Poslednej večeri a pri rozoslaní apoštolov dal však Ježiš niektorým posvätnú moc na službu veriacim. Preto len títo vysvätení kňazi zastupujú Ježiša, Pastiera svojho ľudu a Hlavu svojho tela - Cirkvi. Všetci by sme mali nanovo objaviť Boží dar pre Cirkev, ktorým je vysvätený kňaz, ktorý medzi nami zastupuje samého Pána.
-            </p>
-        </x-web.page.text-segment>
-    </x-web.page.subsection>
+                <p>
+                    Za diakona, kňaza a biskupa môže byť platne vysvätený muž, ktorý prijal katolícku vieru, bol v nej pokrstený a pobirmovaný a je Cirkvou povolaný na tento úrad.
+                </p>
+            </x-web.page.text-segment>
+        </x-web.page.subsection>
 
-    <x-web.page.subsection>
-        <hr class="text-church-template">
-    </x-web.page.subsection>
+        <x-web.page.subsection title="Aký účinok má diakonská vysviacka?">
+            <x-web.page.text-segment animation="fromleft">
 
-    <x-web.page.subsection title="Ako sa niekto môže stať kňazom?">
-        {{-- <x-partials.picture titleSlug="blank" animation="fromright" dimensionSource="full" columns="4" /> --}}
-        <x-web.page.text-segment animation="fromleft">
-            <p>
-                Mladý muž, ktorý by v sebe cítil povolanie ku kňazstvu, môže sa cez svojho farára prihlásiť na štúdium teológie a prípravu na kňazstvo v Banskobystrickej diecéze, v Kňazskom seminári sv. Gorazda v Nitre. Medzi podmienky prijatia adeptov na kňazstvo patrí okrem iného najmä mravný a bezúhonný život kresťana, dobrý zdravotný stav pre výkon povolania a stredoškolské vzdelanie s maturitou. Bližšie informácie na stránke seminára: www.ksnr.sk
-            </p>
-        </x-web.page.text-segment>
-    </x-web.page.subsection>
+                <x-partials.picture titleSlug="sacr6-008" side="left" dimensionSource="medium" columns="3" />
 
-    <x-web.page.information-sources title="Použitá literatúra:">
-        <li>Youcat po slovensky. Katechizmus Katolíckej cirkvi pre mladých. Bratislava: Karmelitánske nakladateľstvo, 2011. ISBN 978-80-89231-99-7.</li>
-    </x-web.page.information-sources>
+                <p>
+                    Diakon je ustanovený pre osobitnú službu Cirkvi. Zastupuje totiž Krista, ktorý „neprišiel dať sa obsluhovať, ale slúžiť a položiť svoj život ako výkupné za mnohých“ (Mt 20,28). V liturgii diakonskej vysviacky sa hovorí: „Posilnený darom Ducha Svätého bude pomáhať biskupovi a jeho kňazom v službe slova, oltára a charity. Stane sa služobníkom všetkých.“ Prvotným vzorom diakona je svätý mučeník Štefan, ktorý bol medzi prvými siedmimi diakonmi, ktorých ustanovili apoštoli, keď sa v službe prvotnej cirkvi v Jeruzaleme cítili preťažení množstvom charitatívnych úloh. Neskôr sa diakonská služba stala na dlhé storočia len nižším stupňom na ceste ku kňazstvu. Dnes sú opäť do tejto služby povolaní jednak tí, ktorí žijú v celibáte, ako aj ženatí muži. Takto chápaná diakonská služba nanovo zdôrazňuje služobný charakter Cirkvi. Zároveň poskytuje kňazom pomoc v duchu prvotnej Cirkvi tým, že diakon prijíma predovšetkým pastoračné a sociálne úlohy v Cirkvi. Aj diakonská vysviacka vtláča celoživotnú a nezrušiteľnú pečať.
+                </p>
+            </x-web.page.text-segment>
+        </x-web.page.subsection>
+
+    </x-web.page.section>
+
+    <x-web.sections.background-picture titleSlug="sacr6-002"/>
+
+    <x-web.page.section name="PAGE: ({{$pageData['title']}}) PART II -" class="static-page pad_t_50 pad_b_50">
+
+        <x-web.page.subsection title="Aký účinok má kňazská vysviacka?">
+            <x-web.page.text-segment animation="fromright">
+
+                <x-partials.picture titleSlug="sacr6-001" side="right" dimensionSource="medium" columns="3" />
+
+                <p>
+                    Pri kňazskej vysviacke Duch Svätý vtláča kňazovi nezmazateľnú pečať, ktorou ho pripodobňuje Kristovi a dáva mu schopnosť konať v Ježišovom mene. Kňaz ako spolupracovník svojho biskupa má hlásať Božie slovo, udeľovať sviatosti a predovšetkým sláviť Eucharistiu.
+                </p>
+
+                <h3 class="mt-2 wow fromright" data-wow-delay="0.4s">
+                    Aký účinok má biskupská vysviacka?
+                </h3>
+
+                <x-partials.picture titleSlug="sacr6-006" side="left" dimensionSource="medium" columns="4" />
+
+                <p>
+                    Biskupskou vysviackou sa udeľuje plnosť sviatosti posvätného stavu. Po vysvätení sa biskup stáva právoplatným nástupcom apoštolov a vstupuje do biskupského zboru. Od okamihu svojho vysvätenia je zodpovedný za celú Cirkev spolu s ostatnými biskupmi a s pápežom. Biskupskou vysviackou sa mu udeľuje poslanie učiť, posväcovať a spravovať. Biskup, ktorý spolu s kňazmi a diakonmi ako so svojimi vysvätenými spolupracovníkmi vykonáva pastiersku službu, je viditeľným princípom a základom miestnej cirkvi (biskupstva/diecézy).
+                </p>
+            </x-web.page.text-segment>
+        </x-web.page.subsection>
+
+        <x-web.page.subsection title="Ako sa odlišuje všeobecné kňazstvo všetkých veriacich od sviatostného kňazstva?">
+            <x-web.page.text-segment animation="fromright">
+                <p>
+                    Každý kresťan je povolaný k všeobecnému kňazstvu, aby v Božom mene pôsobil vo svete a prinášal mu Božie požehnanie a milosť. Pri Poslednej večeri a pri rozoslaní apoštolov dal však Ježiš niektorým posvätnú moc na službu veriacim. Preto len títo vysvätení kňazi zastupujú Ježiša, Pastiera svojho ľudu a Hlavu svojho tela - Cirkvi. Všetci by sme mali nanovo objaviť Boží dar pre Cirkev, ktorým je vysvätený kňaz, ktorý medzi nami zastupuje samého Pána.
+                </p>
+            </x-web.page.text-segment>
+        </x-web.page.subsection>
+
+        {{--
+        <x-web.page.subsection>
+            <hr class="text-church-template">
+        </x-web.page.subsection>
+
+        <x-web.page.subsection title="Ako sa niekto môže stať kňazom?">
+            <x-web.page.text-segment animation="fromleft">
+                <p>
+                    Mladý muž, ktorý by v sebe cítil povolanie ku kňazstvu, môže sa cez svojho farára prihlásiť na štúdium teológie a prípravu na kňazstvo v Banskobystrickej diecéze, v Kňazskom seminári sv. Gorazda v Nitre. Medzi podmienky prijatia adeptov na kňazstvo patrí okrem iného najmä mravný a bezúhonný život kresťana, dobrý zdravotný stav pre výkon povolania a stredoškolské vzdelanie s maturitou. Bližšie informácie na stránke seminára: www.ksnr.sk
+                </p>
+            </x-web.page.text-segment>
+        </x-web.page.subsection> --}}
+
+        <x-web.page.information-sources title="Použitá literatúra:">
+            <li>
+                <em class="me-2">Youcat po slovensky. Katechizmus Katolíckej cirkvi pre mladých.</em>
+                Bratislava: Karmelitánske nakladateľstvo, 2011. ISBN 978-80-89231-99-7.
+            </li>
+        </x-web.page.information-sources>
 
     </x-web.page.section>
 </x-web.layout.master>
