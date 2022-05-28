ac813a7489ad46ed2d311c360cceb1c9c5af5356
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-05-28 02:40:58 +0200

    add wraper to card-event component

--- resources/views/components/partials/cart-event/index.blade.php
+++ resources/views/components/partials/cart-event/index.blade.php
@@ -4,44 +4,45 @@
     'url'   => null,
     'img'   => null,
 ])
-
-<div @class([
-        'event_box d-lg-flex wow',
-        'fromright' => $side === 'right',
-        'fromleft'  => $side !== 'right',
-    ])
->
-
+<div class="ch_about_wrap">
     <div @class([
-            'event_thumb',
-            'order-2' => $side === 'right',
+            'event_box d-lg-flex wow',
+            'fromright' => $side === 'right',
+            'fromleft'  => $side !== 'right',
         ])
     >
-        {!! $img !!}
-    </div>
 
-    <div @class([
-            'event_desc',
-            'order-1' => $side === 'right',
-        ])
-    >
-        <h3>
-            {{ $title }}
-        </h3>
-        <p class="text-justify">
-            {{ $teaser }}
-        </p>
-        <div
-            @class([
-                'd-lg-flex',
-                'justify-content-start' => $side === 'right',
-                'justify-content-end' => $side !== 'right',
+        <div @class([
+                'event_thumb',
+                'order-2' => $side === 'right',
             ])
         >
-            <a href="{{ $url }}" class="event_btn read_btn">
-                Dozvedieť sa viac
-            </a>
+            {!! $img !!}
         </div>
-    </div>
 
+        <div @class([
+                'event_desc',
+                'order-1' => $side === 'right',
+            ])
+        >
+            <h3>
+                {{ $title }}
+            </h3>
+            <p class="text-justify">
+                {{ $teaser }}
+            </p>
+            <div
+                @class([
+                    'd-lg-flex',
+                    'justify-content-start' => $side === 'right',
+                    'justify-content-end' => $side !== 'right',
+                ])
+            >
+                <a href="{{ $url }}" class="event_btn read_btn">
+                    Dozvedieť sa viac
+                </a>
+            </div>
+        </div>
+
+    </div>
 </div>
