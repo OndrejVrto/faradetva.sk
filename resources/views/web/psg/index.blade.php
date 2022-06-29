c3054a5e790986425b56cc8272c85a25f812e969
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-06-29 13:58:48 +0200

    small change format

--- resources/views/web/psg/index.blade.php
+++ resources/views/web/psg/index.blade.php
@@ -7,8 +7,8 @@
 
         <x-web.page.section name="PGS TEST" class="static-page pad_b_80 pad_t_50">
 
-            <div class="d-flex justify-content-center">
-                <div class="col-md-6">
+            <div class="row">
+                <div class="col-12 col-md-6">
                     <form action="{{ route('psg.test') }}">
 
                         <div class="row">
@@ -34,20 +34,22 @@
                             </div>
                         </div>
 
-                        <button class="mt-4 btn btn-primary" type="submit">
-                            Sprav nám Vianoce
-                        </button>
-                        <a href="{{ route('psg.test') }}" class="mt-4 btn btn-secondary">
-                            I feel happy
-                        </a>
+                        <div class="d-flex justify-content-between">
+                            <button class="mt-4 btn btn-primary" type="submit">
+                                Sprav nám Vianoce
+                            </button>
+                            <a href="{{ route('psg.test') }}" class="mt-4 ms-2 btn btn-secondary">
+                                I feel happy
+                            </a>
+                        </div>
                     </form>
                 </div>
-                <div class="col-md-6 mx-5 px-5">
-                    <div style="color: #{{ $color }};">
-                        @foreach ($tree as $layer)
-                            <pre class="my-0">{{ $layer }}</pre>
-                        @endforeach
-                    </div>
+                <div class="col-12 col-md-6 d-flex justify-content-center">
+                    <pre class="fs-5 mt-4 mt-md-0" style="color: #{{ $color }};">
+@foreach ($tree as $layer)
+{{ $layer }}
+@endforeach
+                    </pre>
                 </div>
 
             </div>
--- routes/pgs.php
+++ routes/pgs.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Pgs\StromcekController;
 
-Route::middleware('fast_web')->group(function() {
+// Route::middleware('fast_web')->group(function() {
 
     Route::get('/vianocny_stromcek/{layer?}/{color?}', StromcekController::class)
         ->whereNumber('layer')
         ->where('color', '^([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')
         ->name('psg.test');
 
-});
+// });
