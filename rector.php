37e9eee46f74b97c403170f54240a93af1f2f5b3
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-08-12 20:13:40 +0200

    declare strict types in one row

--- rector.php
+++ rector.php
@@ -1,6 +1,4 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,17 +1,11 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+
     public function test_example() {
         $this->assertTrue(true);
     }
