bf883fa6a038846154eb81d454dbbdf004a8de61
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-11-15 19:59:38 +0100

    refactor migration user

--- database/migrations/2021_11_13_231557_add_column_to_users_table.php
+++ database/migrations/2021_11_13_231557_add_column_to_users_table.php
@@ -7,27 +7,33 @@ use Illuminate\Support\Facades\Schema;
 return new class () extends Migration {
     public function up(): void {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('nick')->unique()->after('name');
-            $table->string('slug')->after('nick');
-            $table->boolean('can_be_impersonated')->after('email')->default(1);
             $table->boolean('active')->after('id')->default(1);
-            $table->string('phone')->nullable()->after('email');
-            $table->string('facebook_url', 512)->nullable()->after('email');
-            $table->string('twitter_url', 512)->nullable()->after('email');
-            $table->string('www_page_url', 512)->nullable()->after('email');
+            $table->after('name', function ($table) {
+                $table->string('nick')->unique();
+                $table->string('slug');
+            });
+            $table->after('email', function ($table) {
+                $table->string('www_page_url', 512)->nullable();
+                $table->string('twitter_url', 512)->nullable();
+                $table->string('facebook_url', 512)->nullable();
+                $table->string('phone')->nullable();
+                $table->boolean('can_be_impersonated')->default(1);
+            });
         });
     }
 
     public function down(): void {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('nick');
-            $table->dropColumn('slug');
-            $table->dropColumn('can_be_impersonated');
-            $table->dropColumn('active');
-            $table->dropColumn('phone');
-            $table->dropColumn('facebook_url');
-            $table->dropColumn('twitter_url');
-            $table->dropColumn('www_page_url');
+            $table->dropColumn([
+                'nick',
+                'slug',
+                'phone',
+                'active',
+                'twitter_url',
+                'facebook_url',
+                'www_page_url',
+                'can_be_impersonated',
+            ]);
         });
     }
 };
