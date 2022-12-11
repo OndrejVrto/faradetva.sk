b20c199730e639bea656a880746145b3c4405428
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2022-12-11 01:37:38 +0100

    add return type to helper

--- app/Http/Helpers/Functions.php
+++ app/Http/Helpers/Functions.php
@@ -2,23 +2,10 @@
 
 use Spatie\Valuestore\Valuestore;
 
-if (!function_exists('random_color_part'))
-{
-    function random_color_part() {
-        return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
-    }
-}
-
-if (!function_exists('random_hex_color'))
-{
-    function random_hex_color() {
-        return random_color_part() . random_color_part() . random_color_part();
-    }
-}
-
 if(!function_exists('customConfig'))
 {
-    function customConfig(string $filename = 'config', $key = null, $default = null): Valuestore|string|null {
+
+    function customConfig(string $filename = 'config', mixed $key = null, mixed $default = null): Valuestore|string|null {
         try {
             $valueStore = Valuestore::make(
                 storage_path("app/value-store/$filename.json")
