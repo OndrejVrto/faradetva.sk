2b9991ce8e4013786411a8d4c06fc844330f105a
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2023-03-25 22:40:08 +0100

    Create new pages for submenu

--- resources/views/web/custom/spolocenstva/index.blade.php
+++ resources/views/web/custom/spolocenstva/index.blade.php
@@ -1,12 +1,10 @@
-<x-web.layout.master :pageData="$pageData" header="off">
+<x-web.layout.master :pageData="$pageData">
 
-    {{-- Spoločenstvá --}}
+    {{-- Spoločenstvá  --}}
 
-    <x-web.page.section name="PAGE: ({{ $pageData->title }}) -" class="ch_ministry_section pad_t_50">
+    <x-web.page.section name="PAGE: ({{ $pageData->title }})" row class="static-page pad_b_50">
 
-        <x-web.page.section-header :header="$pageData['header']" class=""/>
-
-        {{-- <x-partials.page-card routeStaticPages="
+        <x-partials.page-card routeStaticPages="
             spolocenstva.frantiskansky-svetstky-rad,
             spolocenstva.marianske-veceradlo,
             spolocenstva.rad-bosych-karmelitanok,
@@ -16,7 +14,7 @@
             spolocenstva.hnutie-krestanskych-rodin,
             spolocenstva.katolicka-charizmaticka-obnova,
             spolocenstva.sluzobnici-jezisovho-velknazskeho-srdca,
-        " /> --}}
+        " />
 
     </x-web.page.section>
 </x-web.layout.master>
