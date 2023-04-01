9038426e890dc86d202e64393cbdf79f10e5c599
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2023-04-01 16:35:31 +0200

    new content

--- resources/views/web/custom/spolocenstva/index.blade.php
+++ resources/views/web/custom/spolocenstva/index.blade.php
@@ -5,15 +5,19 @@
     <x-web.page.section name="PAGE: ({{ $pageData->title }})" row class="static-page pad_b_50">
 
         <x-partials.page-card routeStaticPages="
+            spolocenstva.index,
+            spolocenstva.spolocenstvo-barka,
             spolocenstva.frantiskansky-svetstky-rad,
-            spolocenstva.marianske-veceradlo,
-            spolocenstva.rad-bosych-karmelitanok,
-            spolocenstva.ruzencove-bratstvo,
-            spolocenstva.svetsky-rad-bosych-karmelitanov,
-            spolocenstva.zdruzenie-salezianskych-spolupracovnikov,
             spolocenstva.hnutie-krestanskych-rodin,
             spolocenstva.katolicka-charizmaticka-obnova,
+            spolocenstva.marianske-veceradlo,
+            spolocenstva.modlitby-matiek,
+            spolocenstva.rad-bosych-karmelitanok,
+            spolocenstva.ruzencove-bratstvo,
+            spolocenstva.saleziansky-spolupracovnici,
             spolocenstva.sluzobnici-jezisovho-velknazskeho-srdca,
+            spolocenstva.svetsky-rad-bosych-karmelitanov,
+            spolocenstva.zdruzenie-faustinum,
         " />
 
     </x-web.page.section>
